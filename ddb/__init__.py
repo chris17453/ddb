5e6b9f2bda9ad30753c8c5dc7e336d8c726935d6
Chris W <nd@atl1l-hgk9vd2.corp.etradegrp.com>
2018-12-04 15:05:57 -0500

    Bump Version

--- ddb/__init__.py
+++ ddb/__init__.py
@@ -5,12 +5,12 @@ import os
 from .engine.sql_engine import sql_engine
 
 #main import
-def engine(config_file=None):
+def engine(config_file=None,mode=='array'):
 
     if None==config_file:
         home = os.path.expanduser("~")
         config_file=os.path.join(os.path.join(home, '.ddb'),'ddb.conf')
-    return sql_engine(config_file=config_file)
+    return sql_engine(config_file=config_file,mode=mode)
 
 
 if __name__ == "__main__":
