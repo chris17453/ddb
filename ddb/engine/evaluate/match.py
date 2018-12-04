c0c3018c9e5b08262133e23167da9f8dc35ac6b3
Chris W <nd@atl1l-hgk9vd2.corp.etradegrp.com>
2018-12-04 15:27:30 -0500

    Bump Version

--- ddb/engine/evaluate/match.py
+++ ddb/engine/evaluate/match.py
@@ -9,7 +9,7 @@ def evaluate_single_match(test,row,table):
     compare1_is_column=False
     compare2_is_column=False
 
-    comparitor=comparitor
+    comparitor=test['c']
 
 
     if None !=comparitor:
