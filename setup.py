3c46fbdbf038498d25192ff26a399cd190de5127
Chris W <nd@localhost.localdomain>
2018-12-05 10:28:12 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ddb',
-    version='1.0.67',
+    version='1.0.68',
     packages=['ddb',],
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
@@ -12,7 +12,7 @@ setup(
     author= 'Charles Watkins',
     author_email= 'charles@titandws.com',
     description= '',
-    install_requires=[
+    install_requires=['pyyaml','flextable'
     ],
     #data_files=[
     #    ('share/icons/hicolor/scalable/apps', ['data/proxx.svg']),
