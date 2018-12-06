7d70cad6b1499f2ee398329ca882e68859171e91
Chris W <nd@localhost.localdomain>
2018-12-05 19:57:31 -0500

    Bump Version

---
+++ ddb/engine/evaluate/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+
+setup(
+    ext_modules = cythonize('match.pyx')
+    
+)
+
+
+
--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
+from distutils.core import setup
+from Cython.Build import cythonize
 from setuptools import setup
+from setuptools.extension import Extension
+
+
+
+extensions = [
+  Extension(
+     "ddb.engine.evaluate.match",
+     [
+         "./ddb/engine/evaluate/match.pyx"
+              ]
+     
+  )]
 
 
 setup(
     name='ddb',
-    version='1.0.81',
+    version='1.0.82',
     packages=['ddb',],
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
@@ -14,6 +28,7 @@ setup(
     description= '',
     install_requires=['pyyaml','flextable'
     ],
+    ext_modules = cythonize(extensions),
     #data_files=[
     #    ('share/icons/hicolor/scalable/apps', ['data/proxx.svg']),
     #    ('share/applications', ['data/proxx.desktop'])
