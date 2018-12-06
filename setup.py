bee0b4ef188ae8d6f124ab9aa9035a604525e5f0
Chris W <nd@localhost.localdomain>
2018-12-05 23:10:48 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ ext = '.py' if USE_CYTHON else '.c'
 
 
 extensions = [
+   Extension("ddb.engine.parser.language",        [ "./ddb/engine/parser/language"+ext ]),
+   Extension("ddb.engine.tokenizer.sql_tokenize", [ "./ddb/engine/tokenizer/sql_tokenize"+ext ]) ,
+   Extension("ddb.engine.parser.sql_parser",      [ "./ddb/engine/parser/sql_parser"+ext ]),
    Extension("ddb.engine.evaluate.match",         [ "./ddb/engine/evaluate/match"+ext ] ),
    Extension("ddb.engine.structure.column",       [ "./ddb/engine/structure/column"+ext ] ),
    Extension("ddb.engine.structure.table",        [ "./ddb/engine/structure/table"+ext ]),
    Extension("ddb.engine.structure.database",     [ "./ddb/engine/structure/database"+ext ]),
    Extension("ddb.engine.sql_engine",             [ "./ddb/engine/sql_engine"+ext ]),
-   Extension("ddb.engine.interactive",            [ "./ddb/engine/interactive"+ext ]),
-   Extension("ddb.engine.parser.language",        [ "./ddb/engine/parser/language"+ext ]),
-   Extension("ddb.engine.parser.sql_parser",      [ "./ddb/engine/parser/sql_parser"+ext ]),
-   Extension("ddb.engine.tokenizer.sql_tokenize", [ "./ddb/engine/tokenizer/sql_tokenize"+ext ]) ,
    Extension("ddb.engine.functions.functions",    [ "./ddb/engine/functions/functions"+ext ]) ,
+   Extension("ddb.engine.interactive",            [ "./ddb/engine/interactive"+ext ]),
    Extension("ddb.cli",                           [ "./ddb/cli"+ext ]) ,
 ]     
   
@@ -35,7 +35,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.112',
+    version='1.0.113',
     packages=['ddb',],
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
