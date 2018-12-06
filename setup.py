4be9a00fd76b48d5263b183c5bb9ffeb22226e72
Chris W <nd@localhost.localdomain>
2018-12-05 23:34:17 -0500

    C files

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.121',
+    version='1.0.122',
     packages=['ddb',],
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
