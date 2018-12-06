ccaf0842b9ab1dd7389aad5d9eb9fa0a7f1272b7
Chris W <nd@localhost.localdomain>
2018-12-05 23:30:36 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.119',
+    version='1.0.120',
     packages=['ddb',],
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
