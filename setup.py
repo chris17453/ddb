607257a8272700301c9b62ec337147d3de3f4419
Chris W <nd@localhost.localdomain>
2018-12-05 20:42:31 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ extensions = [
 
 setup(
     name='ddb',
-    version='1.0.90',
+    version='1.0.91',
     packages=['ddb',],
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
