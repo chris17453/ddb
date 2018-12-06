50e5271b1154967c0a9449fb9e81e872685937fb
Chris W <nd@atl1l-hgk9vd2.corp.etradegrp.com>
2018-12-06 10:39:50 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.131',
+    version='1.0.132',
     packages=['ddb',],
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
@@ -57,8 +57,7 @@ setup(
         [console_scripts]
         ddb = ddb.cli:cli_main
         """ ,
-    cmdclass=dict(
-    ),        
+       
 
     
 )
