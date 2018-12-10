7c1e2a312334454150f062db898da3d0d5558cb7
Chris W <nd@dca1l-4s8wgc2.corp.etradegrp.com>
2018-12-10 07:06:45 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -18,25 +18,25 @@ ext = '.pyx' if USE_CYTHON else '.c'
 extensions = [
    Extension("ddb.engine.parser.language",        [ "./ddb/engine/parser/language"+ext ]),
    Extension("ddb.engine.tokenizer.sql_tokenize", [ "./ddb/engine/tokenizer/sql_tokenize"+ext ]) ,
-   Extension("ddb.engine.parser.sql_parser",      [ "./ddb/engine/parser/sql_parser"+ext ]),
    Extension("ddb.engine.evaluate.match",         [ "./ddb/engine/evaluate/match"+ext ] ),
    Extension("ddb.engine.structure.column",       [ "./ddb/engine/structure/column"+ext ] ),
+   Extension("ddb.engine.functions.functions",    [ "./ddb/engine/functions/functions"+ext ]) ,
+  
+   Extension("ddb.engine.parser.sql_parser",      [ "./ddb/engine/parser/sql_parser"+ext ]),
    Extension("ddb.engine.structure.table",        [ "./ddb/engine/structure/table"+ext ]),
    Extension("ddb.engine.structure.database",     [ "./ddb/engine/structure/database"+ext ]),
    Extension("ddb.engine.sql_engine",             [ "./ddb/engine/sql_engine"+ext ]),
-   Extension("ddb.engine.functions.functions",    [ "./ddb/engine/functions/functions"+ext ]) ,
    Extension("ddb.engine.interactive",            [ "./ddb/engine/interactive"+ext ]),
-   Extension("ddb.cli",                           [ "./ddb/cli"+ext ]) ,
+   #Extension("ddb.cli",                           [ "./ddb/cli"+ext ]) ,
 ]     
   
-
-  
-
 if USE_CYTHON:
     try:
         from Cython.Build import cythonize
     except:
         print ("Cython not found")
+        exit(1)
+    
     extensions =  cythonize(extensions)
 
 setup(
