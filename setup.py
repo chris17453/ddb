278321248ad3c9e5a7da410ace53ac1c5476f506
Chris W <nd@dca1l-4s8wgc2.corp.etradegrp.com>
2018-12-10 08:39:25 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.157',
+    version='1.0.158',
     packages=[  'ddb',
                 'ddb.engine.parser',
                 'ddb.engine.tokenizer',
