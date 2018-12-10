319ea42a6f54ee49e2a1f6727abf1175e575f867
Chris W <nd@dca1l-4s8wgc2.corp.etradegrp.com>
2018-12-10 07:24:45 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.144',
+    version='1.0.145',
     packages=['ddb',],
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
