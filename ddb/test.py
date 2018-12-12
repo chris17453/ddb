d69fc2528691eacd057721c7611c6d2f18b38339
Charles Watkins <charles@titandws.com>
2018-12-12 18:18:24 -0500

    Bump Version 1.0.361)

--- ddb/test.py
+++ ddb/test.py
@@ -10,7 +10,7 @@ class test_engine(unittest.TestCase):
     temp_data='test/MOCK_DATA.csv'
     basedir=os.path.dirname(os.path.abspath(__file__))
 
-    def init(self):
+    def test_a(self):
         config_dir=os.path.join(self.basedir,self.temp_config)
         if os.path.exists(config_dir):
             print config_dir
