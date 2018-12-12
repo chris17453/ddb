fae221358a4cd459f0566f6b11acbbe299f311bd
Charles Watkins <charles@titandws.com>
2018-12-11 19:26:23 -0500

    Bump Version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.188',
+    version='1.0.189',
     packages=[  'ddb',
                 'ddb.engine.parser',
                 'ddb.engine.tokenizer',
