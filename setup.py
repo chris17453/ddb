44ba97986fabace05ca9db27ecab94cfbf6b6310
Charles Watkins <charles@titandws.com>
2018-12-11 22:17:37 -0500

    function support in select, tokenizer updates

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.245',
+    version='1.0.246',
     packages=[  'ddb',
                 'ddb.engine.parser',
                 'ddb.engine.tokenizer',
