7cad12b4a923c7eea0536f65b9cfa0bc75bcb31a
it_username <it_email>
2018-12-12 16:16:03 -0500

    Bump Version 1.0.292

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.292',
+    version='1.0.293',
     packages=[  'ddb',
                 'ddb.engine.parser',
                 'ddb.engine.tokenizer',
