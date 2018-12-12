490cf7fc7d40e274872a04b42e921ddda1d715a4
Charles Watkins <charles@titandws.com>
2018-12-12 18:57:30 -0500

    Update for unit test with variable table

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if USE_CYTHON:
 
 setup(
     name='ddb',
-    version='1.0.394',
+    version='1.0.395',
     packages=[  'ddb',
                 'ddb.engine.parser',
                 'ddb.engine.tokenizer',
