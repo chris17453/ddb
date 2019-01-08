bdac0c89dcc3d43cce7fd350c6e961549228eee8
Charles Watkins <charles@titandws.com>
2019-01-08 12:34:58 -0500

    Bump Version 1.0.525

--- ddb/engine/output/output.pyx
+++ ddb/engine/output/output.pyx
@@ -11,7 +11,28 @@ def format_term(results,output_file):
 
 def format_bash(results,output_file):
     """ouput results data in the bash format"""
-    print(results)
+    data=temp_table.get_results()
+    
+    for c in data.columns:
+    """ouput results data in the bash format"""
+    row_index=0
+    
+    column_index=0
+    for column in data['columns']:
+        print("{0}_columns[{1}]='{2}'".format(name,column_index,column)
+        column_index+=1
+
+
+    for row in data['results']:
+        column_index=0
+        print("{0}_info[{1},error]='{3}'".format(name,row_index,row['error'])
+        print("{0}_info[{1},type]='{3}'".format(name,row_index,row['type'])
+        print("{0}_info[{1},raw]='{3}'".format(name,row_index,row['raw'])
+        for column in row['data']:
+            print("{0}_data[{1},{2}]='{3}'".format(name,row_index,column_index,column)
+            column_index+=1
+        row_index+=1
+    
 
 def format_raw(results,output_file):
     """ouput results data in the yaml format"""
