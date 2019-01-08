bc4f97e253a9f799f28be5dbea117e8400eaed5e
Charles Watkins <charles@titandws.com>
2019-01-08 13:15:31 -0500

    Bump Version 1.0.547

--- ddb/engine/sql_engine.pyx
+++ ddb/engine/sql_engine.pyx
@@ -271,8 +271,8 @@ class sql_engine:
             match_results=False
 
 
-        
-        return {'data': line_data, 'type': line_type, 'raw': line, 'line_number': line_number, 'match': match_results, 'error': err}
+        # raw has rstrip for line.. maybe configuration option? Extra data anyway...
+        return {'data': line_data, 'type': line_type, 'raw': line_cleaned, 'line_number': line_number, 'match': match_results, 'error': err}
 
     def select(self, query_object, parser):
         temp_data = []
