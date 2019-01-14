10bc9c4ba5039a481a2b43910c23578983c36848
Charles Watkins <charles@titandws.com>
2019-01-14 10:56:14 -0500

    keyword update

--- src_build/core/ddb_core.py
+++ src_build/core/ddb_core.py
@@ -24,7 +24,7 @@ import tempfile
 
 
 
-__version__='1.0.625'
+__version__='1.0.626'
 
         
         
@@ -870,9 +870,6 @@ class tokenizer():
             '!',  # not
             '|',  # or
 
-            'not',  # not
-            'is',  # equality
-            'like',  # partial match
 
             '+',  # addition
             '-',  # subtraction
@@ -941,8 +938,9 @@ class tokenizer():
             for d in delimiters_sorted:
                 delimter_len = len(d)
                 fragment = text[c:c + delimter_len]
-                if c >= text_length - 1:
-                    self.info("Last Cycle")
+
+
+
                 if True == self.compare_text_fragment(fragment, d) or c >= text_length - 1:
                     self.info("Delemiter found", c, fragment)
                     if c - word_start > 0:
