fe365ede0abcf258c09e69bf15d4dcdc73335999
Charles Watkins <charles@titandws.com>
2019-01-17 16:18:45 -0500

    top down yaml decoding

--- ddb/engine/output/emitter.py
+++ ddb/engine/output/emitter.py
@@ -177,16 +177,6 @@ class obj_formatter():
             line="".join(str1)
         return line
 
-    def yaml_indent_changed(self,s):
-        if s['indent']>s['curent_indent']:
-            print("Exit recursion. Indent dropped")
-            return True
-        if s['indent']<s['curent_indent'] and s['changed']==1:
-            print("Exit recursion. Indent increased")
-            return True
-        return None
-
-
     def yaml_get_tuple(self,line):
         index=line.find(':')
         if index==-1:
@@ -200,216 +190,179 @@ class obj_formatter():
             data=None
         return {'key':key,'data':data}
 
-    def recurse(self,s):
-        temp_s=s
-        if isinstance(s['obj'],list):
-            temp_s['in_array']=True
-        else:
-            temp_s['in_array']=None
-        
-        yaml_data=self.yaml_load(temp_s)
-        
-        if isinstance(s['obj'],list):
-            print("Its a list")
-            s['obj'].append(yaml_data['results'])
-        elif isinstance(s['obj'],object):
-            if s['tuple']==None:
-                raise Exception("tuple key needed: {0}".format(yaml_data['results']))
-            s['obj'][s['tuple']['key']]=yaml_data
-        return s
 
-
-    def update_indent(self,s):
-        if s['indent']==None or (s['indent']!=s['curent_indent'] and s['changed']==0):
-                s['changed']=1
-                s['indent']=s['curent_indent']
-        return s
 
     # initally pass a string,
     # after which it becomes a dict holding eveything
     
-    def yaml_load(self,s):
-        #print("Recursion: depth: {0},indent:{1}, index:{2}".format(depth,indent,start_index))
-        if isinstance(s,str):
-            lines=s.splitlines()
-            s={}
-            s['lines']=lines
-            s['length']=len(s['lines'])
-            s['index']=0
-            s['indent']=0
-            s['process_objects']=True
-            s['process_arrays']=True
-        s['changed']=0
-        s['tuple']=None
-        s['line_cleaned']=None
-        s['obj']={}
 
-
-        while s['index']  < s['length']:
-            recurse=None
-            s['line']=s['lines'][s['index']]
-            s['line_cleaned']=s['line'].strip()
-            s['curent_indent']=self.get_indent(s['line'])
-
-            if self.yaml_is_start(s['line']):
-                s['index']+=1
-                continue
-
-            if self.yaml_is_end(s['line']):
-                return s
-
-       
-            if s['process_objects']:
-                if ':' in s['line_cleaned']:
-                    s['tuple']=self.yaml_get_tuple(s['line_cleaned'])
-                    print(s['tuple'])
-
-                    # cat get here/.. just sayin
-                    if s['tuple']==None:
-                        raise Exception("Tuple invalid")
-
-                    # No value in the tuple, lets recurse and populate with sub object
-                    if s['tuple']['data']:
-                        if isinstance(s['obj'],list):
-                            print("Its a list")
-                            s['obj'].append(s['tuple']['data'])
-                        elif isinstance(s['obj'],object):
-                            s['obj'][s['tuple']['key']]=s['tuple']['data']
-                    else:
-                        recurse=True
-                    s['index']+=1
-                
-                
-                # print "*",key,data,"*"
-                # its not an list or object, its a simple element of a list
-                else:
-                    s['index']+=1
-                    #if isinstance(obj,list):
-                    #    print ("Exit recursion. single entity")
-                    #   obj.append(line_cleaned)
-                    s['results']=s['line_cleaned']
-                    return s
-                    #    return obj
-                    #elif isinstance(obj,object):
-                    #    obj[key]=data
-                #    if not data:        
-
-            # if anything has happened above, an object has been created.
-            # now we search for the object and append it...
-
-            s=self.update_indent(s)
-            #    recurse=True
-
-            if self.yaml_indent_changed(s) or recurse:
-                s=self.recurse(s)
-        # end of loop 
-
-        print ("Exit recursion. END")
-        return s
- 
- 
+    def yaml_load(self,data):
+        print("Loading")
+        lines=data.splitlines()
         
-#            # this is reached after a recursion from a tuple. so no key
-#            # always make it a list override and ignore all else    
-#            if 1==1:
-#                if self.yaml_is_array(line_cleaned):
-#                    #if in_array and start_index!=index:
-#                    #    print("Exit recursion. next item in list")
-#                    #    self.temp_yaml_index=index
-#                    #    return obj
-#                    #print("in array")
-#                    
-#                    #curent_indent=self.get_indent(line)
-#                    
-#                    lines[index]=self.yaml_strip_array(line)
-#                    line_cleaned=lines[index].strip()
-#                    
-#                    if not isinstance(obj,list):
-#                        print ("reset list")
-#                        obj=[]
-#
-#                    obj=self.recurse(lines,index,depth+1,curent_indent,recurse,yaml_tuple,obj)
-#                    continue
-#
-#                          
-#            #    #index+=01
-#            #    #print "KEY",key
-#            #    obj.append(self.yaml_load(lines,index=index,indent=curent_indent+1,depth=depth+1,list_depth=list_depth+1))
-#            #    index=self.temp_yaml_index
-#            #    #return obj
-#            #    list_skip=True
-#            
-#            
-#            
-#            #if list_skip:
-#            #    print("depth: {0},indent:{1}, index:{2}: cur_indent:{3}, changed:{4}".format(depth,indent,index,curent_indent,changed))
-#            #curent_indent=self.get_indent(line)
-#            
-#            # because i dont want to put the above code in twice...
-#            #if list_skip:
-#            #    continue
-#            
-#            if 1==1:
-#                if ':' in line_cleaned:
-#                    yaml_tuple=self.yaml_get_tuple(line_cleaned)
-#
-#                    print yaml_tuple
-#
-#                    # cat get here/.. just sayin
-#                    if yaml_tuple==None:
-#                        raise Exception("Tuple invalid")
-#
-#                    # No value in the tuple, lets recurse and populate with sub object
-#                    if yaml_tuple['data']:
-#                        if isinstance(obj,list):
-#                            print("Its a list")
-#                            obj.append(yaml_tuple['data'])
-#                        elif isinstance(obj,object):
-#                            obj[yaml_tuple['key']]=yaml_tuple['data']
-#                    else:
-#                        recurse=True
-#                    index+=1
-#                
-#                
-#                # print "*",key,data,"*"
-#                # its not an list or object, its a simple element of a list
-#                else:
-#                    index+=1
-#                    #if isinstance(obj,list):
-#                    self.temp_yaml_index=index+1
-#                    #    print ("Exit recursion. single entity")
-#                    #   obj.append(line_cleaned)
-#                    return line_cleaned
-#                    #    return obj
-#                    #elif isinstance(obj,object):
-#                    #    obj[key]=data
-#                #    if not data:
-#
-#
-#            # if anything has happened above, an object has been created.
-#            # now we search for the object and append it...
-#
-#            if indent==None or (indent!=curent_indent and changed==0):
-#                changed=1
-#                indent=curent_indent
-#            #    recurse=True
-#
-#            if self.yaml_indent_changed(s) or recurse:
-#                temp_s=self.recurse(s)
-#                
-#                return s
+        root={}
+        last_indent=None
+        group={}
+        line_number=1
+        obj=root
+        hash_map=[{'indent':0,'obj':obj}]
+        in_array=False
+        feed_me_an_object=None
+
+        for line in lines:
+            if self.yaml_is_start(line):
+                print("Starting")
+                continue
+            if self.yaml_is_end(line):
+                print("End")
+                break
+            indent=self.get_indent(line)
+
+            if feed_me_an_object and last_indent==indent:
+                raise Exception("Empty tuple before: {0}:{1}".format(line_number,line))
+
+            print("THIS  indent {0}: last indent: {1}".format(indent,last_indent))
+            line_cleaned=line.strip()
+            is_array=self.yaml_is_array(line_cleaned)
+            line_cleaned=self.yaml_strip_array(line_cleaned)
             
 
+            # ok, forward walking is automatic, and requires no map.
+            # when an indernt drops, the map is popped
+
+            if  not isinstance(obj,list) and is_array:
+                print ("In new array setup")
+                in_array=True
+                print obj
+                obj[last_tuple['key']]=[]
+                # now the mind warp, take me back to some linked list action here
+                # repoint the object brah
+                obj=obj[last_tuple['key']]
+                
+                hash_map.append({'indent':indent,'obj':obj})
+                
+
+
+            if 0==0:
+                if last_indent and  last_indent>indent or is_array:
+                    #    obj={}
+                    #else:
+                    #    obj={}
+                    found_it=None
+                    #print "Hash"
+                    #print hash_map
+                    #print "ROOT"
+                    #print (root)
+                    print "*",line
+                    for index in range(len(hash_map)-1,-1,-1):
+                        #print index
+                        offset=0
+                        if is_array:
+                            print("is array")
+                            if hash_map[index]['indent']==indent:
+                                #print(hash_map)
+                                print("Found indent {0}".format(hash_map[index]['indent']))
+                                obj=hash_map[index]['obj']
+                                print (obj)
+                                found_it=True
+                                break;
+                        else:
+                            print("is not array")
+                            if hash_map[index]['indent']<=indent:
+                                #print(hash_map)
+                                print("Found indent {0}".format(hash_map[index]['indent']))
+                                obj=hash_map[index]['obj']
+                                print (obj)
+                                found_it=True
+                                break;
+                    if isinstance(obj,list):
+                        print("its a list, make array")
+                        in_array=True
+                    else:
+                        print("its a list, REMOVE array")
+                        in_array=None
+                    if found_it == None:
+                        print ("Didnt Find it")
+                        pprint(root)
+                    else:
+                        feed_me_an_object=None
+
+
+
+            
+
+                            
+   
+            # is it a tuple?
+            line_tuple=self.yaml_get_tuple(line_cleaned)
+            if line_tuple:
+                if None == obj:
+                    raise Exception("Object leak")
+                print (line_tuple)
+
+                if line_tuple['data'] and isinstance(obj,list):
+                   feed_me_an_object=True 
+
+                if feed_me_an_object:
+                    if isinstance(obj,list):
+                        a={}
+                        obj.append(a)
+                        # now the mind warp, take me back to some linked list action here
+                        # repoint the object brah
+                        obj=a
+                    else:
+                        print last_tuple
+                        obj[last_tuple['key']]={}
+                        # now the mind warp, take me back to some linked list action here
+                        # repoint the object brah
+                        obj=obj[last_tuple['key']]
+                    hash_map.append({'indent':indent,'obj':obj})
+                    feed_me_an_object=None
+
+
+                # ok its a tuple... and it has data. lets just add it.
+                if line_tuple['data']:
+                    print (line_tuple['key'])
+                    obj[line_tuple['key']]=line_tuple['data'].strip()
+                    feed_me_an_object=None
+
+                 #well darn, no data. guess the next object is the data...
+                else:
+                     
+                     feed_me_an_object=True
+                last_tuple=line_tuple
+            else:
+                print("Not tuple")
+                if isinstance(obj,list):
+                    print ("adding thing")
+                    obj.append(line_cleaned.strip())
+                    feed_me_an_object=None
+                else:
+                    raise Exception("Cant have free standing data with no parent")
+
+
+            line_number+=1
+            last_indent=indent
+            
+        print ("Exit recursion. END")
+        return root
+ 
+
+
 data={}
-#data['array']=[0,2,3,4]
+data['array']=[0,2,3,4]
 data['group']={}
-#data['sam']['fred']=[6,7,8]
-#data['sam']['sam']=[9,8,7,6,5,4,3,2,1,0,3,4,3]
+data['array2']={}
+data['array2']['arr1']=[6,7,8]
+data['array2']['arr2']=[9,8,7,6,"number",4,3,2,1,0,3,4,3]
 op={}
 op['sddc']='roc'
 op['vcenter']="1"
 op['cloudgw']=1.2
-#data['group']['operations']=op
+data['group']['operations']=op
+#data['arr']=[]
+#data['arr'].append([2,3,4])
+#data['arr'].append([5,6,7])
 data['list']=[]
 o={}
 o['key']='data'
@@ -423,15 +376,15 @@ data['list'].append(o)
 of=obj_formatter()
 #json_data = of.render_json(data)
 #xml_data  = of.render_xml(data,root='object')
-yaml_data = of.render_yaml(data,indent=2)
+yaml_data = of.render_yaml(data,indent=5)
 #print json_data
 #print xml_data
 print yaml_data
 
 yaml_object=of.yaml_load(yaml_data)
 
-print "----------"
-print "----------"
+print "----------X"
+print "----------X"
 
 
 pprint(yaml_object)
