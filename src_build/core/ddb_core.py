ca7664df5295139e464304b292f4327795a378fc
Charles Watkins <charles@titandws.com>
2019-02-04 18:23:53 -0500

    Bump Version 1.0.693

--- src_build/core/ddb_core.py
+++ src_build/core/ddb_core.py
@@ -23,7 +23,7 @@ import tempfile
 
 
 
-__version__='1.0.692'
+__version__='1.0.693'
 
         
         
--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from .. import ddb
 
 
