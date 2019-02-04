c46cb37b164c0860bceaa70d96e7a50f95c0517c
Charles Watkins <charles@titandws.com>
2019-02-04 10:49:57 -0500

    Bump Version 1.0.655

--- ansible/ddb.py
+++ ansible/ddb.py
@@ -24,7 +24,7 @@ import tempfile
 
 
 
-__version__='1.0.654'
+__version__='1.0.655'
 
         
         
@@ -2627,12 +2627,14 @@ class sql_engine:
     def use(self, query_object):
         self.info("Use")
         target_db = query_object['meta']['use']['table']
-        self.database.set_database(target_db)
-        temp_table = self.database.temp_table()
-        temp_table.add_column('changed_db')
-        data = {'data': [target_db], 'type': self.data_type.DATA, 'error': None}
-        temp_table.append_data(data)
-        return temp_table
+        if self.database.get_curent_database()!=target_db:
+            self.database.set_database(target_db)
+            temp_table = self.database.temp_table()
+            temp_table.add_column('changed_db')
+            data = {'data': [target_db], 'type': self.data_type.DATA, 'error': None}
+            temp_table.append_data(data)
+            return temp_table
+        return None
 
     def create_table(self, query_object):
         self.info("Create Table")
