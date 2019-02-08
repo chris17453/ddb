0f172791f60e7411906b5d1d523b31f02308e0fa
Charles Watkins <charles@titandws.com>
2019-02-07 20:45:00 -0500

    Bump Version 1.0.784

--- setup.py
+++ setup.py
@@ -11,46 +11,32 @@ if '--use-cython' in sys.argv:
     prefix=''
     print("Using Cython")
     USE_CYTHON=True
-else:
-    ext = '.py'
-    prefix='' 
-    USE_CYTHON=None
-
-
-extensions = [
-    Extension("ddb.evaluate.match",                [prefix+"./ddb/evaluate/match" + ext]),
-    Extension("ddb.functions.functions",           [prefix+"./ddb/functions/functions" + ext]),
-    Extension("ddb.lexer.language",                [prefix+"./ddb/lexer/language" + ext]),
-    Extension("ddb.lexer.tokenize",                [prefix+"./ddb/lexer/tokenize" + ext]),
-    Extension("ddb.lexer.lexer",                   [prefix+"./ddb/lexer/lexer" + ext],
-                                      include_dirs=[prefix+'./ddb/structure/'],
-              ),
-    Extension("ddb.structure.column",              [prefix+"./ddb/structure/column" + ext]),
-    Extension("ddb.structure.table",               [prefix+"./ddb/structure/table" + ext]),
-    Extension("ddb.structure.database",            [prefix+"./ddb/structure/database" + ext]),
-    Extension("ddb.output.factory",                [prefix+"./ddb/output/factory" + ext], ),
-    Extension("ddb.output.factory_yaml",           [prefix+"./ddb/output/factory_yaml" + ext], ),
-    Extension("ddb.output.factory_xml",            [prefix+"./ddb/output/factory_xml" + ext], ),
-    Extension("ddb.output.factory_json",           [prefix+"./ddb/output/factory_json" + ext], ),
-    Extension("ddb.version",                       [prefix+"./ddb/version" + ext], ),
-    Extension("ddb.engine",                        [prefix+"./ddb/engine" + ext], ),
-    Extension("ddb.interactive",                   [prefix+"./ddb/interactive" + ext], ),
-#    Extension("ddb.cli", ["./ddb/cli" + ext], ),
-]
-
-
-if USE_CYTHON:
+    extensions = [
+        Extension("ddb.evaluate.match",                [prefix+"./ddb/evaluate/match" + ext]),
+        Extension("ddb.functions.functions",           [prefix+"./ddb/functions/functions" + ext]),
+        Extension("ddb.lexer.language",                [prefix+"./ddb/lexer/language" + ext]),
+        Extension("ddb.lexer.tokenize",                [prefix+"./ddb/lexer/tokenize" + ext]),
+        Extension("ddb.lexer.lexer",                   [prefix+"./ddb/lexer/lexer" + ext],
+                                        include_dirs=[prefix+'./ddb/structure/'],
+                ),
+        Extension("ddb.structure.column",              [prefix+"./ddb/structure/column" + ext]),
+        Extension("ddb.structure.table",               [prefix+"./ddb/structure/table" + ext]),
+        Extension("ddb.structure.database",            [prefix+"./ddb/structure/database" + ext]),
+        Extension("ddb.output.factory",                [prefix+"./ddb/output/factory" + ext], ),
+        Extension("ddb.output.factory_yaml",           [prefix+"./ddb/output/factory_yaml" + ext], ),
+        Extension("ddb.output.factory_xml",            [prefix+"./ddb/output/factory_xml" + ext], ),
+        Extension("ddb.output.factory_json",           [prefix+"./ddb/output/factory_json" + ext], ),
+        Extension("ddb.version",                       [prefix+"./ddb/version" + ext], ),
+        Extension("ddb.engine",                        [prefix+"./ddb/engine" + ext], ),
+        Extension("ddb.interactive",                   [prefix+"./ddb/interactive" + ext], ),
+    #    Extension("ddb.cli", ["./ddb/cli" + ext], ),
+    ]
     try:
         from Cython.Build import cythonize
     except BaseException:
         print ("No Cython installed")
         exit(1)
     extensions = cythonize(extensions)
-
-exec(open('ddb/version.py').read())
-setup(
-    name='ddb',
-    version=__version__,
     packages=['ddb',
               'ddb.lexer',
               'ddb.evaluate',
@@ -58,7 +44,22 @@ setup(
               'ddb.functions',
               'ddb.structure',
               'ddb.output',
-              ],
+              ]
+else:
+    ext = '.py'
+    prefix='' 
+    USE_CYTHON=None
+    extensions=None
+    packages=['ddb']
+
+
+
+
+exec(open('ddb/version.py').read())
+setup(
+    name='ddb',
+    version=__version__,
+    packages=packages,
     include_package_data=True,
     url='https://github.com/chris17453/ddb/',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
