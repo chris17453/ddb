37e49056181f5a2ee2e4bf074ddada1ab59cd71f
Charles Watkins <charles@titandws.com>
2019-02-21 15:34:43 -0500

    Bump Version 1.0.775

--- source/ddb/engine.py
+++ source/ddb/engine.py
@@ -138,7 +138,7 @@ class engine:
             if query_object['mode'] == 'describe table':
                 self.results = method_describe_table(self,query_object)
 
-        if ! isinstance(self.results,table):
+        if is not isinstance(self.results,table):
             return self.results
 
         # only return last command
