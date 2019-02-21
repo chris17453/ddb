71cadf7aa9bed274ecce8cdddd7f655aca02348a
Charles Watkins <charles@titandws.com>
2019-02-21 13:32:05 -0500

    Bump Version 1.0.770

--- source/conf/bump.sh
+++ source/conf/bump.sh
@@ -7,7 +7,6 @@ v=$(cat $raw_version_file)
 echo "VERION="$v
 #put it in the main coinfig file
 echo "${v%.*}.$((${v##*.}+1))">$raw_version_file
-exit
 
 #update the version in the py file
 echo "__version__='${v%.*}.$((${v##*.}+1))'">$python_version_file
