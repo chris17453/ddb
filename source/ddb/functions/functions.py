a55c4d5ef99a76fa99151a6c481855eb5053e755
Charles Watkins <charles@titandws.com>
2019-02-21 15:38:00 -0500

    Bump Version 1.0.778

--- source/ddb/functions/functions.py
+++ source/ddb/functions/functions.py
@@ -3,34 +3,50 @@ import datetime
 
 
 
-def f_database(context,database):
-    return database.get_curent_database()
+def f_database(context):
+    if None==context:
+        raise Exception("No database instance. ")
+    return context.database.get_curent_database()
 
 def f_upper(context,arg):
+    if None==context:
+        raise Exception("No database instance. ")
     if not arg:
         return None
     return arg.upper()
 
 def f_lower(context,arg):
+    if None==context:
+        raise Exception("No database instance. ")
     if not arg:
         return None
     return arg.lower()
 
 def f_datetime(context,arg=None):
+    if None==context:
+        raise Exception("No database instance. ")
     return datetime.datetime.now()
 
 def f_time(context,arg=None):
+    if None==context:
+        raise Exception("No database instance. ")
     return datetime.datetime.now().strftime('%H:%M:%S')
 
 def f_date(context,arg=None):
+    if None==context:
+        raise Exception("No database instance. ")
     return datetime.datetime.now().strftime('%Y-%m-%d')
 
 def f_version(context,version=None):
+    if None==context:
+        raise Exception("No database instance. ")
     if None==version:
         return 'GA.BB.LE'
     return version
         
 def f_cat(context,arg1,arg2):
+    if None==context:
+        raise Exception("No database instance. ")
     if None ==arg1:
         arg1=''
     if None ==arg2:
