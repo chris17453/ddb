ec2f30b7b8326e781336c28be07e2fb27aa1660c
Charles Watkins <charles@titandws.com>
2019-02-21 13:31:53 -0500

    Bump Version 1.0.769

--- source/conf/bump.sh
+++ source/conf/bump.sh
@@ -5,9 +5,9 @@ python_version_file='./source/ddb/version.py'
 v=$(cat $raw_version_file)
 
 echo "VERION="$v
-exit
 #put it in the main coinfig file
 echo "${v%.*}.$((${v##*.}+1))">$raw_version_file
+exit
 
 #update the version in the py file
 echo "__version__='${v%.*}.$((${v##*.}+1))'">$python_version_file
