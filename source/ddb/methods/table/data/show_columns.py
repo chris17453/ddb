2706bb2d62b37031f633fc2a82d43ee5c7d4f4ff
Charles Watkins <charles@titandws.com>
2019-02-21 13:20:59 -0500

    Bump Version .1

--- source/ddb/methods/table/data/show_columns.py
+++ source/ddb/methods/table/data/show_columns.py
@@ -4,6 +4,6 @@ def method_show_columns(context,database, query_object):
     temp_table = database.temp_table(columns=['table', 'column'])
 
     for c in table.columns:
-        columns = {'data': [table.data.name, c.data.name], 'type': context.DATA, 'error': None}
+        columns = {'data': [table.data.name, c.data.name], 'type': context.data_type.DATA, 'error': None}
         temp_table.append_data(columns)
     return temp_table
