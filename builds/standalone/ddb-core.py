0b1cab0ec4fa2030659bc80edff0c398908da3c4
Charles Watkins <charles@titandws.com>
2019-02-22 10:06:35 -0500

    Bump Version 1.0.819

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.818'
+__version__='1.0.819'
 
         
         
--- source/test/test.py
+++ source/test/test.py
@@ -95,6 +95,7 @@ class test_engine(unittest.TestCase):
             results = engine.query('select * from {} where id="1" order by id LIMIT 100;'.format(self.table_name))
             print ("55")
             self.assertEqual(True, results.success)
+            print ("56")
             self.assertEqual(1, len(results.data))
             
             # WHERE AND/LIMIT
