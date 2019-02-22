d01c9c982b22531c23c2844b382f56a3e71b3371
Charles Watkins <charles@titandws.com>
2019-02-22 10:05:51 -0500

    Bump Version 1.0.816

--- source/test/test.py
+++ source/test/test.py
@@ -93,6 +93,7 @@ class test_engine(unittest.TestCase):
 
             # WHERE/LIMIT
             results = engine.query('select * from {} where id="1" order by id LIMIT 100;'.format(self.table_name))
+            print ("55")
             self.assertEqual(True, results.success)
             print ("55")
             self.assertEqual(1, len(results.data))
