65969f3780083f3a03150a9cc8da5be775996b07
Charles Watkins <charles@titandws.com>
2019-02-22 09:55:49 -0500

    Bump Version 1.0.807

--- source/ddb/methods/table/core.py
+++ source/ddb/methods/table/core.py
@@ -79,5 +79,6 @@ class query_results:
         self.success=success
         self.affected_rows=affected_rows
         self.data=data
+        print(data)
         self.error=None
         print("Success: {0} Error:{1}".format(success,error))
