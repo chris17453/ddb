220fc7a5b18e986d5f058a78460ce143f757cfe0
Charles Watkins <charles@titandws.com>
2019-02-22 09:47:23 -0500

    Bump Version 1.0.800

--- source/test/test.py
+++ source/test/test.py
@@ -75,7 +75,7 @@ class test_engine(unittest.TestCase):
             engine = ddb.engine(config_file=os.path.join(self.basedir, self.temp_config))
             # fail on existing table
             results = engine.query("create table {}('id','first_name','last_name','email','gender','ip_address') file='{}'".format(self.table_name, os.path.join(self.basedir, self.temp_data)))
-            self.assertEqual(False, results.success)
+            self.assertEqual(True, results.success)
             # test results length
             results = engine.query('select * from {} LIMIT 10'.format(self.table_name))
             self.assertEqual(True, results.success)
