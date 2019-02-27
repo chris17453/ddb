4687b2c6780d5b631e176ce69c14d7886b709651
Charles Watkins <chris17453@gmail.com>
2019-02-27 11:27:29 -0500

    Bump Version 1.0.889

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.888'
+__version__='1.0.889'
 
         
         
@@ -1842,7 +1842,6 @@ class database:
                      whitespace=None,
                      data_on=None,
                      temporary=None):
-        print("Creating table")
         if None == database_name:
             database_name = self.get_curent_database()
         exists = self.get(table_name, database_name)
@@ -1874,10 +1873,9 @@ class database:
             res = t.save()
             if False == res:
                 raise Exception("Couldn't save table configuation")
-        else:
-            print("Not saving temporary table")
+        print( "33")
         self.add_config(table=t)
-
+        print( "SS")
         self.reload_config()
         return True
 
