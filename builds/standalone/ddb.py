a426a36c2027bed2fad9cbf7b36d4eea54797cc3
Charles Watkins <chris17453@gmail.com>
2019-02-27 13:29:55 -0500

    Bump Version 1.0.937

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.936'
+__version__='1.0.937'
 
         
         
@@ -2665,6 +2665,7 @@ def method_select(context, query_object, parser):
         
 
         if 'order by' in query_object['meta']:
+             print ("Order by",query_object['meta']['ordinals'])
             context_sort = []
             for c in query_object['meta']['order by']:
                 ordinal = query_object['meta']['ordinals'][c['column']]
