35e07d9bf1535feaf24d452bd55429e713641bfc
Charles Watkins <chris17453@gmail.com>
2019-02-27 12:37:40 -0500

    Bump Version 1.0.919

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.918'
+__version__='1.0.919'
 
         
         
@@ -1776,9 +1776,7 @@ class database:
         """Get a Table structure in the database."""
         if None == database_name:
             database_name = self.get_curent_database()
-        print("Matching  {0}".format(database_name))
         for c in self.tables:
-            print("Using {0}.{1} matching {2}.{3}".format(c.data.database,c.data.name, database_name,table_name))
             if c.data.name == table_name and database_name == c.data.database:
                 return c
         return None
