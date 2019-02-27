72dc7426f4c39ae0795b18219fac4dc69d762834
Charles Watkins <chris17453@gmail.com>
2019-02-27 11:06:07 -0500

    Bump Version 1.0.880

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.879'
+__version__='1.0.880'
 
         
         
@@ -3164,9 +3164,9 @@ class output_factory:
                     if 'raw' in row:
                         write_file.write(row['raw'])
 
-    def format_yaml(self,temp_table,output_file):
+    def format_yaml(self,query_results,output_file):
         """ouput results data in the yaml format"""
-        results=temp_table.get_results()
+        results=results.data
         factory=factory_yaml()
         dump=factory.dump(results)
         if not output_file:
@@ -3175,9 +3175,9 @@ class output_factory:
             with open(output_file, "w") as write_file:
                 write_file.write(dump)
 
-    def format_json(self,temp_table,output_file):
+    def format_json(self,query_results,output_file):
         """ouput results data in the json format"""
-        results=temp_table.get_results()
+        results=results.data
         factory=factory_json()
         dump=factory.dumps(results)
         if not output_file:
@@ -3186,9 +3186,9 @@ class output_factory:
             with open(output_file, "w") as write_file:
                 write_file.write(dump)
         
-    def format_xml(self,temp_table,output_file):
+    def format_xml(self,query_results,output_file):
         """ouput results data in the xml format"""
-        results=temp_table.get_results()
+        results=results.data
         factory=factory_xml()
         dump=factory.dumps({'data':results})
         if not output_file:
