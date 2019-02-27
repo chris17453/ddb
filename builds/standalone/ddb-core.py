6dc4bd08e09a9d5455827a1b00c0abbe754d9e2b
Charles Watkins <chris17453@gmail.com>
2019-02-27 12:27:49 -0500

    Bump Version 1.0.913

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.912'
+__version__='1.0.913'
 
         
         
@@ -677,7 +677,7 @@ class lexer:
                                         if None == parent:
                                             if meta_type=='single':
                                                 for arg_key in curent_object['arguments']:
-                                                    query_object[arg_key] = curent_object[arg_key]
+                                                    query_object['arguments'][arg_key] = curent_object[arg_key]
                                             else:    
                                                 query_object[curent_object['mode']] = curent_object['arguments']
                                             self.info("NO APPEND")
