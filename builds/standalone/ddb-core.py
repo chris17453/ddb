1a35c15e241466b8a8b1d5dd8026af0fc48a7c02
Charles Watkins <chris17453@gmail.com>
2019-02-27 13:25:11 -0500

    Bump Version 1.0.931

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.930'
+__version__='1.0.931'
 
         
         
@@ -2641,20 +2641,21 @@ def method_select(context, query_object, parser):
         ordinals={}
         index=0
         for column in query_object['meta']['columns']:
-            print(column)
             if 'display' in column:
                 name=column['display']
             elif  'function' in column:
                 name=column['function']
             elif 'column' in column:
                 name=column['column']
-
+            else:
+                continue
             ordinals[name]=index
             index+=1
+
         query_object['meta']['ordinals']=ordinals
         
 
-        if '3order by' in query_object['meta']:
+        if 'order by' in query_object['meta']:
             print ("Order by")
             context_sort = []
             for c in query_object['meta']['order by']:
