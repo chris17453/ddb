8ce16e9025d66cc57ce3fcdc763f0bac63d75bf1
Charles Watkins <chris17453@gmail.com>
2019-02-27 11:59:03 -0500

    Bump Version 1.0.894

--- source/test/test.py
+++ source/test/test.py
@@ -154,10 +154,10 @@ class test_engine(unittest.TestCase):
         self.cleanup()
         try:
             engine = ddb.engine(config_file=os.path.join(self.basedir, self.temp_config))
-
+            print("Create")
             results = engine.query("create table {} ('id','first_name','last_name','email','gender','ip_address') file='{}'".format(self.table_name, os.path.join(self.basedir, self.temp_data)))
             self.assertEqual(True, results.success)
-
+            priunt ("insert")
             results = engine.query("insert into {} ('id','first_name','last_name','email','gender','ip_address') values (1003,test_name,test_lname,'bop@bob.com','m','0.0.0.0')".format(self.table_name))
             self.assertEqual(True, results.success)
             
