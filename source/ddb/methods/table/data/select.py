19188dbee165f384a6cb5a7de480dcf7e356f616
Charles Watkins <chris17453@gmail.com>
2019-02-27 13:26:06 -0500

    Bump Version 1.0.933

--- source/ddb/methods/table/data/select.py
+++ source/ddb/methods/table/data/select.py
@@ -107,7 +107,7 @@ def method_select(context, query_object, parser):
         
 
         if 'order by' in query_object['meta']:
-            print ("Order by")
+            print ("Order by",query_object['meta']['ordinals'])
             context_sort = []
             for c in query_object['meta']['order by']:
                 ordinal = query_object['meta']['ordinals'][c['column']]
