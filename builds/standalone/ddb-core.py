5fb0eb180acf08dc594c9db440134e3a32c8a8ef
Charles Watkins <chris17453@gmail.com>
2019-02-27 10:46:52 -0500

    Bump Version 1.0.871

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.870'
+__version__='1.0.871'
 
         
         
@@ -343,10 +343,7 @@ sql_syntax = {
               'name': 'create',
               'optional': False
               },
-             {'data': None,
-              'name': 'temporary',
-              'optional': True
-              },
+
              {'arguments': 1,
               'data': [{'sig': ['{table}']}],
               
