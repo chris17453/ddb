7ec43deb6bd233e9f87910434a4ac4a095c66b68
Charles Watkins <chris17453@gmail.com>
2019-02-27 12:01:35 -0500

    Bump Version 1.0.899

--- source/test/test.py
+++ source/test/test.py
@@ -157,9 +157,11 @@ class test_engine(unittest.TestCase):
             print("Create")
             results = engine.query("create table {} ('id','first_name','last_name','email','gender','ip_address') file='{}'".format(self.table_name, os.path.join(self.basedir, self.temp_data)))
             self.assertEqual(True, results.success)
+            results = engine.query("show tables")
+            print(results.data,results.success,results.error)
+            
             print ("insert")
             results = engine.query("insert into {} ('id','first_name','last_name','email','gender','ip_address') values (1003,test_name,test_lname,'bop@bob.com','m','0.0.0.0')".format(self.table_name))
-            print(results.data,results.success,results.error)
             self.assertEqual(True, results.success)
             print("Delete")
             # delete just inserted
