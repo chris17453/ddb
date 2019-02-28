f19b3a138f47756162c45491f1d679c9ccb41692
Charles Watkins <charles@titandws.com>
2019-02-28 17:03:53 -0500

    Bump Version 1.0.964

--- source/ddb/methods/table/data/select.py
+++ source/ddb/methods/table/data/select.py
@@ -58,7 +58,7 @@ def select_process_file(context,query_object):
     has_columns = select_has_columns(context,query_object)
     file_path = query_object['table'].data.path
     line_number = 1
-
+    data=[]
     if True == has_columns:
         with open(file_path, 'r') as content_file:
             for line in content_file:
