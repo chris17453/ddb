5a127914938cc307e233986642e7df9fde385eb3
Charles Watkins <charles@titandws.com>
2019-02-28 17:01:53 -0500

    Bump Version 1.0.961

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.960'
+__version__='1.0.961'
 
         
         
@@ -2604,6 +2604,7 @@ def method_select(context, query_object, parser):
 
         return query_results(success=True,data=temp_table)
     except Exception as ex:
+        print ex
         return query_results(success=False,error=ex)   
 
 
