c711956cfe0db65b0e420daf1c09ded46e58c401
Charles Watkins <chris17453@gmail.com>
2019-03-01 07:38:29 -0500

    Bump Version 1.0.995

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -43,7 +43,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.994'
+__version__='1.0.995'
 
         
         
@@ -2589,7 +2589,6 @@ context_sort=[]
 def method_select(context, query_object, parser):
     try:
         context.info(query_object)
-        print ("START")
         select_validate_columns_and_from(context,query_object,parser)
 
         temp_table = context.database.temp_table()
--- source/test/test.py
+++ source/test/test.py
@@ -149,17 +149,14 @@ class test_engine(unittest.TestCase):
         self.cleanup()
         try:
             engine = ddb.engine(config_file=os.path.join(self.basedir, self.temp_config))
-            print("Create")
             results = engine.query("create table {} ('id','first_name','last_name','email','gender','ip_address') file='{}'".format(self.table_name, os.path.join(self.basedir, self.temp_data)))
             self.assertEqual(True, results.success)
 
             results = engine.query("show tables")
             print(results.data,results.success,results.error)
             
-            print ("insert")
             results = engine.query("insert into {} ('id','first_name','last_name','email','gender','ip_address') values (1003,test_name,test_lname,'bop@bob.com','m','0.0.0.0')".format(self.table_name))
             self.assertEqual(True, results.success)
-            print("Delete")
             # delete just inserted
             results = engine.query("delete from {} where id='1003'".format(self.table_name))
             self.assertEqual(True, results.success)
@@ -169,7 +166,6 @@ class test_engine(unittest.TestCase):
             self.assertEqual(True, results.success)
         except Exception as ex:
             print(ex)
-            print ("HI")
             self.fail(ex)
 
 
