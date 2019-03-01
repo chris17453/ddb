0ee8336e83f80813aeae4a520f7f97ca171fdb78
Charles Watkins <chris17453@gmail.com>
2019-03-01 07:24:27 -0500

    Bump Version 1.0.979

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.978'
+__version__='1.0.979'
 
         
         
@@ -2447,10 +2447,8 @@ def method_use(context, query_object):
             temp_table.add_column('changed_db')
             data = {'data': [target_db], 'type': context.data_type.DATA, 'error': None}
             temp_table.append_data(data)
-        print "success"
         return query_results(success=True,data=temp_table)
     except Exception as ex:
-        print ex
         return query_results(success=False,error=ex)
         
         
