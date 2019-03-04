ea289b2f596d26552aee67fbac2ba9fb8c031d8b
Charles Watkins <chris17453@gmail.com>
2019-03-04 09:31:55 -0500

    Bump Version 1.0.1033

--- source/ddb/methods/table/structure/drop.py
+++ source/ddb/methods/table/structure/drop.py
@@ -6,4 +6,5 @@ def method_drop_table(context, query_object):
         results = context.database.drop_table(table_name=query_object['meta']['drop']['table'])
         return query_results(success=results)
     except Exception as ex:
+        print("EHG",ex)
         return query_results(success=False,error=ex)
