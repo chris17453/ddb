610182374d7ecb8f5f5ac236b32fc6080949364d
Charles Watkins <chris17453@gmail.com>
2019-03-04 09:16:02 -0500

    Bump Version 1.0.1029

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.1028'
+__version__='1.0.1029'
 
         
         
--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -43,7 +43,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.1028'
+__version__='1.0.1029'
 
         
         
