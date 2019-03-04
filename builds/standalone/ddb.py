90b1e19e97d787731ef35869def130fa6cb5ed45
Charles Watkins <chris17453@gmail.com>
2019-03-04 09:34:04 -0500

    Bump Version 1.0.1036

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -43,7 +43,7 @@ except Exception as ex:
 
 
 
-__version__='1.0.1035'
+__version__='1.0.1036'
 
         
         
@@ -1915,10 +1915,6 @@ class database:
             database_name = self.get_curent_database()
         for index in range(0, len(self.tables)):
             if self.tables[index].data.name == table_name and self.tables[index].data.database == database_name:
-                if self.tables[index].data.type=="Temp":
-                    self.tables.pop(index)
-                    self.reload_config()
-                    return True
 
                 res = self.remove_config(table_object=self.tables[index])
                 if False == res:
--- source/ddb/methods/table/structure/drop.py
+++ source/ddb/methods/table/structure/drop.py
@@ -3,7 +3,9 @@ from ..core import query_results
 def method_drop_table(context, query_object):
     context.info("Drop Table")
     try:
-        results = context.database.drop_table(table_name=query_object['meta']['drop']['table'])
+        table_name=query_object['meta']['drop']['table']
+        print (table_name)
+        results = context.database.drop_table(table_name=table_name)
         return query_results(success=results)
     except Exception as ex:
         print("EHG",ex)
