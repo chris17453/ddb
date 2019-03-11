4301c87fa237fc4a255cc0b63c34dcd945d599a8
Charles Watkins <chris17453@gmail.com>
2019-03-11 13:02:14 -0400

    Bump Version 1.1.4

--- builds/pypi/ddb-1.0.1066/ddb/methods/database/show_errors.py
+++
@@ -1,8 +0,0 @@
-from ..table.core import query_results
-
-def method_show_errors(context, query_object):
-    context.info("Use")
-    try:
-        return query_results(success=True)
-    except Exception as ex:
-        return query_results(success=False,error=ex)
--- builds/pypi/ddb-1.1.3/README.md
+++
@@ -1,5 +0,0 @@
-# ddb source
-
--This is the source directory for ddb.
--Do not use this.
--This source builds the usaable source in the ../builds directory
