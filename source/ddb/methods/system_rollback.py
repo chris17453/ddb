ecd1407c9d83c5079bdaf2431db9028ea1090c9a
Charles Watkins <charles@titandws.com>
2019-03-12 15:36:57 -0400

    Bump Version 1.1.122

---
+++ source/ddb/methods/system_rollback.py
@@ -0,0 +1,8 @@
+from .record_core import query_results
+
+def method_system_rollback(context, query_object):
+    context.info("set")
+    try:
+        return query_results(success=True)
+    except Exception as ex:
+        return query_results(success=False,error=ex)
