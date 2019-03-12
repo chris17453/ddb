ef387eef02a8e1bdbb2363b5fc84f9bdb1c82bb9
Charles Watkins <charles@titandws.com>
2019-03-12 15:33:53 -0400

    Bump Version 1.1.120

--- source/ddb/methods/system_set.py
+++ source/ddb/methods/system_set.py
@@ -3,8 +3,8 @@ from .record_core import query_results
 def method_system_set(context, query_object):
     context.info("set")
     try:
-        variable=query_object['meta']['variable']
-        value=query_object['meta']['value']
+        variable=query_object['meta']['set']['variable']
+        value=query_object['meta']['set']['value']
         if variable in context.system:
             context.system[variable]=value
         else:
