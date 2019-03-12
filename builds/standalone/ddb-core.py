5af65fb50f7d52755954f3a2f11bffe93a6d96cb
Charles Watkins <chris17453@gmail.com>
2019-03-12 07:40:09 -0400

    Bump Version 1.1.25

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ except Exception as ex:
 
 
 
-__version__='1.1.24'
+__version__='1.1.25'
 
         
         
@@ -858,6 +858,9 @@ class lexer:
             self.info(switch_index, token_index, len(tokens))
 
             self.info(curent_object)
+            if token_index == len(tokens):
+                break
+
         
         
         result=self.validate(curent_object,tokens,token_index,switch,query,switch_index,query_object,query_mode)
