f3d0581b009749d78c9eed0a6bad5080c501e9d9
Charles Watkins <chris17453@gmail.com>
2019-03-12 10:11:11 -0400

    Bump Version 1.1.82

--- source/ddb/methods/table_create.py
+++ source/ddb/methods/table_create.py
@@ -41,7 +41,6 @@ def method_create_table(context, query_object):
             found_errors = query_object['meta']['errors']
         if 'data_starts_on' in query_object['meta']:
             found_data_on = query_object['meta']['data_starts_on']
-            print("FIND DATA STARTS ON",found_data_on )
             
         results = context.database.create_table(table_name=query_object['meta']['table'],
                                                 database_name=database_name,
