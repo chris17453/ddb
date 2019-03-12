05a2f200f8e76af57f1e151308a64acb55bb89d5
Charles Watkins <chris17453@gmail.com>
2019-03-12 09:59:31 -0400

    Bump Version 1.1.79

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -44,7 +44,7 @@ except Exception as ex:
 
 
 
-__version__='1.1.78'
+__version__='1.1.79'
 
         
         
@@ -424,7 +424,7 @@ sql_syntax = {
         {'query': 'drop',
          'switch': [{'arguments': 1,
                      'data': [ {'sig': ['{table}']},
-                               {'sig': ['{database}','.','{table}']}]
+                               {'sig': ['{database}','.','{table}']}],
 
                      'name': 'drop'}]},
         {'query': 'create',
