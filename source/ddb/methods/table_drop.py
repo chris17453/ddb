6bf9eb70070781bffd27cbf3dcf65c71735c56d4
Charles Watkins <chris17453@gmail.com>
2019-03-12 10:22:43 -0400

    Bump Version 1.1.93

--- source/ddb/methods/table_drop.py
+++ source/ddb/methods/table_drop.py
@@ -4,9 +4,9 @@ def method_drop_table(context, query_object):
     context.info("Drop Table")
     try:
         table_name=query_object['meta']['drop']['table']
-        if 'database' in query_object['meta']:
+        if 'database' in query_object['meta']['drop']:
             context.info('Database specified')
-            database_name = query_object['meta']['database']
+            database_name = query_object['meta']['drop']['database']
         else:
             context.info('Using curent database context')
             database_name = context.database.get_curent_database()
