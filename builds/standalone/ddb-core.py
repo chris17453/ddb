c6278c1746565c8f750496521305527d2cd5b7e3
Charles Watkins <chris17453@gmail.com>
2019-03-14 13:43:50 -0400

    Bump Version 1.1.147

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -36,7 +36,7 @@ except Exception as ex:
 
 
 
-__version__='1.1.146'
+__version__='1.1.147'
 
         
         
@@ -3775,7 +3775,7 @@ class flextable:
 
 
     class flextable_style:
-        def __init__(self,style='single'):
+        def __init__(self,style='rst'):
             self.whitespace=''
             self.line_ending='LRCF'
             self.color=flextable.modes()
