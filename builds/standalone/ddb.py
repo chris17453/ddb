c01e26e2a6e770fecfa742fef4fd40bd8d26baad
Charles Watkins <chris17453@gmail.com>
2019-03-14 13:45:25 -0400

    rst for flextable

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -44,7 +44,7 @@ except Exception as ex:
 
 
 
-__version__='1.1.147'
+__version__='1.1.148'
 
         
         
@@ -3995,7 +3995,7 @@ class flextable:
     data_type=enum(COMMENT=1,ERROR=2,DATA=3,WHITESPACE=4)
     
     def __init__(self,      data,
-                            display_style='single',
+                            display_style='rst',
                             column_count=0,
                             hide_comments=False,
                             hide_errors=False,
