7725b83edbacd5ab580d151c49415593b9968c82
Charles <chris17453@gmail.com>
2019-03-17 06:44:38 -0400

    Bump Version 1.1.159

--- source/ddb/output/factory_term.py
+++ source/ddb/output/factory_term.py
@@ -559,7 +559,7 @@ class flextable:
         row=self.style.characters.center.left.render(use_color=self.render_color)
         
         for c in range(0,self.column_count):
-            row+=self.style.color.default.render('x',use_color=self.render_color,length=self.column_character_width)
+            row+=self.style.color.default.render('',fill_character=self.characters.center.center,use_color=self.render_color,length=self.column_character_width)
             row+=self.style.characters.center.right.render(use_color=self.render_color)
         
         row+=u'{}'.format(flextable.reset.ALL)
