b530bd7308053dd171aadb8bfe12df39d1343a9e
Charles <chris17453@gmail.com>
2019-03-17 06:17:04 -0400

    Bump Version 1.1.151

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -44,7 +44,7 @@ except Exception as ex:
 
 
 
-__version__='1.1.150'
+__version__='1.1.151'
 
         
         
@@ -2777,7 +2777,6 @@ def method_select(context, query_object, parser):
 
         return query_results(success=True,data=temp_table)
     except Exception as ex:
-        print ex
         return query_results(success=False,error=ex)   
 
 
