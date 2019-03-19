7ad8d6a6857ab750adc519277eaf9ff238373cb6
Charles Watkins <chris17453@gmail.com>
2019-03-19 12:51:34 -0400

    Bump Version 1.1.276

--- source/ddb/methods/system_set.py
+++ source/ddb/methods/system_set.py
@@ -33,5 +33,5 @@ def method_system_set(context, query_object):
 
         return query_results(success=True)
     except Exception as ex:
-        print(ex)
+        #print(ex)
         return query_results(success=False,error=ex)
