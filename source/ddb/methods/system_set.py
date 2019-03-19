d93e1ba08e5c56fa85d0f576f87f9b5abb57e33b
Charles Watkins <chris17453@gmail.com>
2019-03-19 12:23:00 -0400

    Bump Version 1.1.265

--- source/ddb/methods/system_set.py
+++ source/ddb/methods/system_set.py
@@ -9,24 +9,22 @@ def method_system_set(context, query_object):
             variable=item['variable'].upper()
             value=item['value']
             value_up=value.upper()
-            null_array=['NULL','NILL','NONE']
-            false_array=['FALSE','NO']
-            true_array=['TRUE','YES']
-            #if value_up in false_array:
-            #    value=False
-            #elif value_up in true_array:
-            #    value=True
-#
-            #elif value_up in null_array:
-            #    value=None
+
+            if value_up in ['FALSE','NO']:
+                value=False
+            elif value_up in ['TRUE','YES']:
+                value=True
+
+            elif value_up in ['NULL','NILL','NONE']:
+                value=None
 
             if var_type=='system':
                 if variable in context.system:
                     context.system[variable]=value
                 else:
                     raise Exception("Cannot set {0}, not a system variable".format(variable))
-            elif var_type=='user':
-                context.user[variable]=value
+            #elif var_type=='user':
+            #    context.user[variable]=value
 
         return query_results(success=True)
     except Exception as ex:
