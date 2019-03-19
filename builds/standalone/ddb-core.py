efe289adfbeafbc9aa05fcb1e38f95826ea4a6e2
Charles Watkins <chris17453@gmail.com>
2019-03-19 09:22:53 -0400

    Bump Version 1.1.198

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -36,7 +36,7 @@ except Exception as ex:
 
 
 
-__version__='1.1.197'
+__version__='1.1.198'
 
         
         
@@ -2675,6 +2675,7 @@ def method_delete(context, query_object):
                     if True == processed_line['match']:
                         affected_rows += 1
                         continue
+                    print ("Writing")
                     temp_file.write(processed_line['raw'])
                     temp_file.write(query_object['table'].delimiters.get_new_line())
                 swap_files(data_file, temp_file.name)
