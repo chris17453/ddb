f2a17fa0ce4bd50c3a3d14d44a94fe829a2d3d3b
Charles Watkins <chris17453@gmail.com>
2019-03-19 09:25:09 -0400

    Bump Version 1.1.202

--- source/ddb/methods/record_delete.py
+++ source/ddb/methods/record_delete.py
@@ -19,6 +19,7 @@ def method_delete(context, query_object):
         with open(temp_data_file, 'r') as content_file:
             print("opened file")
             with tempfile.NamedTemporaryFile(mode='w+b', prefix=temp_file_prefix,delete=True) as temp_file:
+                print("opened other file")
                 for line in content_file:
                     processed_line = process_line(context,query_object, line, line_number)
                     if None != processed_line['error']:
