e4568f95327d5a5ce16df3081e27f400c8657efa
Charles <chris17453@gmail.com>
2019-03-18 21:46:04 -0400

    Bump Version 1.1.176

--- source/ddb/__init__.py
+++ source/ddb/__init__.py
@@ -7,12 +7,12 @@ from .engine import engine as ddb_engine
 # main import
 
 
-def engine(config_file=None, mode='array'):
+def engine(config_file=None, mode='array',debug=False):
 
     if config_file is None:
         home = os.path.expanduser("~")
         config_file = os.path.join(os.path.join(home, '.ddb'), 'ddb.conf')
-    return ddb_engine(config_file=config_file, mode=mode)
+    return ddb_engine(config_file=config_file, mode=mode,debug=debug)
 
 
 if __name__ == "__main__":
