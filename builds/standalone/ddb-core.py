56d9bf0c3de2e59e86292e612ff04cf22dc6ce2d
Charles <chris17453@gmail.com>
2019-03-18 21:45:05 -0400

    Bump Version 1.1.175

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -36,7 +36,7 @@ except Exception as ex:
 
 
 
-__version__='1.1.174'
+__version__='1.1.175'
 
         
         
@@ -2600,10 +2600,9 @@ def create_temporary_copy(path,prefix):
         
         temp_path = os.path.join(temp_dir, temp_file_name)
         shutil.copy2(path, temp_path)
-        
+        print("Deleting: {0} Copying to Deleted: {1}".format(path,temp_path))
         return temp_path
     except Exception as ex:
-        print ex
         raise Exception("Temp File Error: {0}".format(ex))
         
 def swap_files(path, temp):
