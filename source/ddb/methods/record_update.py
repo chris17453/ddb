2240fcc7976633c3acd4ef44f0e3431a683b2a85
Charles Watkins <chris17453@gmail.com>
2019-03-19 10:47:19 -0400

    Bump Version 1.1.229

--- source/ddb/methods/record_update.py
+++ source/ddb/methods/record_update.py
@@ -78,6 +78,7 @@ def method_update(context, query_object):
                         continue
                     temp_file.write(processed_line['raw'])
                     temp_file.write(query_object['table'].delimiters.get_new_line())
+                    temp_file.flush()
                 swap_files(data_file, temp_file.name)
 
         return query_results(affected_rows=affected_rows,success=True)
