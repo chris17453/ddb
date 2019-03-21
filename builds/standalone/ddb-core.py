8f2bba413101b48b9d06ea8c9ae25dee8f75f234
Charles Watkins <chris17453@gmail.com>
2019-03-21 11:42:37 -0400

    Bump Version 1.1.294

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -34,7 +34,7 @@ import time
 
 
 
-__version__='1.1.293'
+__version__='1.1.294'
 
         
         
@@ -2383,7 +2383,7 @@ class engine:
         self.output_file=output_file
         self.match=match()
         self.system={}
-        system_trigger={}
+        self.system_trigger=[]
         self.internal={}
         
         self.system['DEBUG']=False
