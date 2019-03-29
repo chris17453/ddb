f0df6653d1a1f6e9b53cf0e168f6f50542e6d2cf
Charles Watkins <chris17453@gmail.com>
2019-03-29 10:52:48 -0400

    Test update

--- source/ddb/methods/database_use.py
+++ source/ddb/methods/database_use.py
@@ -6,11 +6,13 @@ def method_use(context, query_object):
         target_db = query_object['meta']['use']['table']
         if context.database.get_curent_database()!=target_db:
             context.database.set_database(target_db)
-            temp_table = context.database.temp_table()
-            temp_table.add_column('changed_db')
-            data = {'data': [target_db], 'type': context.data_type.DATA, 'error': None}
-            temp_table.append_data(data)
+
+        temp_table = context.database.temp_table()
+        temp_table.add_column('changed_db')
+        data = {'data': [target_db], 'type': context.data_type.DATA, 'error': None}
+        temp_table.append_data(data)
         # TODO so.. is this how we want to handle actions not preformed?
         return query_results(success=True,data=temp_table)
     except Exception as ex:
+        print ex
         return query_results(success=False,error=ex)
