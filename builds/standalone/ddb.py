e2b76ea7a92bc541465f91a83bbac983890c0f6a
Charles Watkins <chris17453@gmail.com>
2019-04-15 10:01:51 -0400

    Bump Version 1.1.358

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -41,7 +41,7 @@ from os.path import expanduser
 
 
 
-__version__='1.1.357'
+__version__='1.1.358'
 
         
         
@@ -451,7 +451,8 @@ sql_syntax = {
                      'parent': 'where'}]},
 
         {'query': 'upsert',
-         'switch': [{'data': False, 'name': 'upsert'},
+         'switch': [
+                     {'data': False, 'name': 'upsert'},
                         {'arguments': 1,
                         'data': [ {'sig': ['{table}']},
                                   {'sig': ['{database}','.','{table}']},
@@ -478,12 +479,13 @@ sql_syntax = {
                      'data': [{'sig': ['{column}']}],
                      'name': ['on','duplicate','key'],
                      'optional': True,
-                     'store_array': True},
+                     }
+                     ,
                     {'arguments': 0,
                      'data': [{'sig': ['{column}', '=', '{expression}']}],
                      'name': ['update'],
                      'optional': True,
-                     'store_array': True},
+                     },
                     {'arguments': 1,
                      'data': [{'sig': ['{e1}', '{c}', '{e2}']}],
                      'name': 'where',
