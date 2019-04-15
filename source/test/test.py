b4b382e89d865387ead051cbbff16cb53c46d479
Charles Watkins <chris17453@gmail.com>
2019-04-15 10:58:55 -0400

    Bump Version 1.1.367

--- source/test/test.py
+++ source/test/test.py
@@ -190,8 +190,8 @@ class test_engine(unittest.TestCase):
         self.cleanup()
         try:
             engine = ddb.engine(config_file=os.path.join(self.basedir, self.temp_config),debug=True)
-            #results = engine.query("create table {} ('id','first_name','last_name','email','gender','ip_address') file='{}'".format(self.table_name, os.path.join(self.basedir, self.temp_data)))
-            #self.assertEqual(True, results.success)
+            results = engine.query("create table {} ('id','first_name','last_name','email','gender','ip_address') file='{}'".format(self.table_name, os.path.join(self.basedir, self.temp_data)))
+            self.assertEqual(True, results.success)
 
             results = engine.query("upsert into {} ('id','first_name','last_name','email','gender','ip_address') values (1001,test_name,test_lname,'bop@bob.com','m','0.0.0.0') ON DUPLICATE KEY id UPDATE id='10000' ".format(self.table_name))
             self.assertEqual(True, results.success)
