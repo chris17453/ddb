97708c0b8a0b0e87d5b38f4e3d53b1ba70e6dad0
Charles Watkins <chris17453@gmail.com>
2019-04-16 10:57:36 -0400

    Bump Version 1.1.553

--- source/ddb/methods/record_upsert.py
+++ source/ddb/methods/record_upsert.py
@@ -71,9 +71,9 @@ def method_upsert(context, query_object):
                     context.info("row found in upsert")
 
                 temp_file.flush()
-                swap_files(data_file, temp_file.name)
+                #swap_files(data_file, temp_file.name)
         
-        #remove_temp_file(temp_data_file)      
+        remove_temp_file(temp_data_file)      
                 
 
         return query_results(affected_rows=affected_rows,success=True)
