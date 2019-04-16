11cbb8a3a8ade66f77a5e39e5941521ebb7bea5c
Charles Watkins <chris17453@gmail.com>
2019-04-16 10:31:55 -0400

    Bump Version 1.1.527

--- source/test/test.py
+++ source/test/test.py
@@ -33,7 +33,7 @@ class test_engine(unittest.TestCase):
         except Exception as ex:
             self.fail(ex)
 
-    def test_create_table(self):
+    def t1est_create_table(self):
         """Test creating a table"""
         try:
             self.cleanup()
@@ -48,7 +48,7 @@ class test_engine(unittest.TestCase):
         results=engine.query("create table {} ('id','first_name','last_name','email','gender','ip_address') file='{}'".format(self.table_name, os.path.join(self.basedir, self.temp_data)))
         self.assertEqual(False, results.success)
 
-    def test_drop_table(self):
+    def t1est_drop_table(self):
         """Test dropping a table"""
         self.cleanup()
         engine = ddb.engine(config_file=os.path.join(self.basedir, self.temp_config))
@@ -105,7 +105,7 @@ class test_engine(unittest.TestCase):
         except Exception as ex:
             self.fail(ex)
 
-    def test_update(self):
+    def t1est_update(self):
         """Update a row in the test file"""
         try:
             self.cleanup()
@@ -127,7 +127,7 @@ class test_engine(unittest.TestCase):
             print(ex)
             self.fail(ex)
 
-    def test_insert(self):
+    def t1est_insert(self):
         """Insert a row in the test file"""
         try:
             self.cleanup()
@@ -147,7 +147,7 @@ class test_engine(unittest.TestCase):
         except Exception as ex:
             self.fail(ex)
 
-    def test_delete(self):
+    def t1est_delete(self):
         """Delete a test row in the test file"""
         self.cleanup()
         try:
