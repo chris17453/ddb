1f76e9bc6241d86829f044302df537654f120ade
Charles Watkins <chris17453@gmail.com>
2019-04-16 09:57:34 -0400

    Bump Version 1.1.521

--- source/ddb/methods/record_core.py
+++ source/ddb/methods/record_core.py
@@ -124,6 +124,10 @@ def swap_files(path, temp):
 
         shutil.copy2(temp, norm_path)
         
+        os.remove(temp)
+        if os.path.exists(temp):
+            raise Exception("Deleting temp file {0} failed".format(temp))
+        
     except Exception as ex:
         raise Exception("File Error: {0}".format(ex))
  
