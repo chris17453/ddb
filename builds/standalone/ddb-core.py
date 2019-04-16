60c4bbddd3260865c303b5e412af009d337bb8ee
Charles Watkins <chris17453@gmail.com>
2019-04-16 08:44:14 -0400

    Bump Version 1.1.504

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -34,7 +34,7 @@ import time
 
 
 
-__version__='1.1.503'
+__version__='1.1.504'
 
         
         
--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -41,7 +41,7 @@ from os.path import expanduser
 
 
 
-__version__='1.1.503'
+__version__='1.1.504'
 
         
         
