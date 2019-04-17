7b8bc566da6f166e015865757d095b6db498c595
Charles Watkins <chris17453@gmail.com>
2019-04-17 10:37:52 -0400

    Bump Version 1.1.637

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -34,7 +34,7 @@ import time
 
 
 
-__version__='1.1.636'
+__version__='1.1.637'
 
         
         
--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -41,7 +41,7 @@ from os.path import expanduser
 
 
 
-__version__='1.1.636'
+__version__='1.1.637'
 
         
         
