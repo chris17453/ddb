657a3f1162c279bb6d0074008ddb2d480212af3d
Charles Watkins <chris17453@gmail.com>
2019-04-22 08:36:25 -0400

    Bump Version 1.1.674

--- source/ddb/__init__.py
+++ source/ddb/__init__.py
@@ -2,14 +2,8 @@ from cli import cli_main
 import os
 
 
-from .engine import engine as ddb_engine
+from .engine import engine
 
-# main import
-
-
-def engine(config_file=None, mode='array',debug=False):
-
-    return ddb_engine(config_file=config_file,mode=mode,debug=debug)
 
 
 if __name__ == "__main__":
