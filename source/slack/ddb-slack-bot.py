6c47663849697fc05f9f7767b8e92bf15fdc8f03
Charles Watkins <chris17453@gmail.com>
2019-04-24 08:21:51 -0400

    Bump Version 1.1.698

--- source/slack/ddb-slack-bot.py
+++ source/slack/ddb-slack-bot.py
@@ -60,7 +60,7 @@ class ddb_bot:
     # declare engine    
     e=engine(config_file=self.DDB_CONFIG)
     # run query
-    query=return_my_message(msg)
+    query=self.return_my_message(msg)
     results=e.query(query)
     # format results
     output_factory(results,output=e.system['OUTPUT_MODULE'],output_style=e.system['OUTPUT_STYLE'],output_file=None)
