7af206ca05b8008799d0e7351925076ef57be517
Charles Watkins <chris17453@gmail.com>
2019-04-24 08:39:48 -0400

    Bump Version 1.1.705

--- source/ddb/output/factory.py
+++ source/ddb/output/factory.py
@@ -39,11 +39,11 @@ class output_factory:
                 self.output=self.format_term(query_results,output_file)
 
 
-    def format_term(self,query_results,output_file,output_style=None):
+    def format_term(self,query_results,output_file,output_style=None,output_stream=None):
         """ouput results data in the term format"""
         #try:
         if query_results.columns:
-            res=flextable(data=query_results.data,columns=query_results.columns,display_style=output_style)
+            res=flextable(data=query_results.data,columns=query_results.columns,display_style=output_style,output_stream=output_stream)
         if True == query_results.success:
             res.append("executed in {0:.6f}, {1} rows returned".format(query_results.time,query_results.data_length))
         else:
