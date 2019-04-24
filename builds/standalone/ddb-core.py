e40078b1d23c13e554f7b3785d0bbca5d5371992
Charles Watkins <chris17453@gmail.com>
2019-04-24 08:51:38 -0400

    Bump Version 1.1.716

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -31,7 +31,7 @@ import time
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.1.715'
+__version__='1.1.716'
 
         
 # ############################################################################
@@ -3186,7 +3186,10 @@ class output_factory:
     def format_term(self,query_results,output_file,output_style=None,output_stream=None):
         """ouput results data in the term format"""
         if query_results.columns:
-            res=flextable(data=query_results.data,columns=query_results.columns,display_style=output_style,output_stream=output_stream)
+            ft=flextable(data=query_results.data,columns=query_results.columns,display_style=output_style,output_stream=output_stream)
+            res=ft.output_destination
+        else:
+            res=[]
         if True == query_results.success:
             res.append("executed in {0:.6f}, {1} rows returned".format(query_results.time,query_results.data_length))
         else:
