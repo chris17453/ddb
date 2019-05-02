e0e155ddc10de5a56d7ef99c26393f7e60d47c5f
Charles Watkins <chris17453@gmail.com>
2019-05-02 11:55:29 -0400

    Bump Version 1.1.895

--- source/ddb/engine.py
+++ source/ddb/engine.py
@@ -79,8 +79,8 @@ class engine:
         self.internal={}
         self.internal={'READONLY':readonly,'TEMP_FILES':{}}
         # variables that can be set by the system
-        uuid_str=str(uuid.uuid1())
-        self.system['UUID']=uuid_str
+        uuid_str=uuid.uuid1()
+        self.system['UUID']= uuid.urn
         self.system['DEBUG']=False
         self.system['AUTOCOMMIT']=True
         self.system['OUTPUT_MODULE']=output
