fde0f32662711c88a062cd5354d468164696e147
Charles Watkins <chris17453@gmail.com>
2019-05-02 11:44:45 -0400

    Bump Version 1.1.886

--- source/ddb/file_io/locking.py
+++ source/ddb/file_io/locking.py
@@ -79,7 +79,7 @@ class lock:
         lock_cycle=0
         while 1:
             lock_status=lock.is_locked(path,uuid)
-            if lock_status>lock.LOCK_OWNER:
+            if lock_status<lock.LOCK_OTHER:
                 break
             lock.info("Lock","File locked, waiting till file timeout, or max lock retry time, {0},{1},{2}".format(path,lock_time,lock_status))
 
