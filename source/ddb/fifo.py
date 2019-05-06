9b38d47e4409eddbfaf19f3fb7cf09fafe8329c7
Charles Watkins <charles@titandws.com>
2019-05-05 20:48:40 -0400

    fifo

--- source/ddb/fifo.py
+++ source/ddb/fifo.py
@@ -10,7 +10,7 @@ class ddb_passthrough(threading.Thread):
     
     def __init__(self, group=None, target=None, name=None,args=(), kwargs=None, verbose=None):
         threading.Thread.__init__(self, group=group, target=target, name=name,verbose=verbose)
-        self.setDaemon(True)
+        # self.setDaemon(True)
         self.args = args
         self.kwargs = kwargs
         return
@@ -19,14 +19,31 @@ class ddb_passthrough(threading.Thread):
         print(self.args)
         thread_name=self.getName()
         logging.debug("{0} started".format(thread_name))
-        FIFO = self.kwargs['src']
+        FIFO = os.path.expanduser(self.kwargs['src'])
         #os.mkfifo(FIFO)
         while True:
+            logging.debug("{0} Looping".format(thread_name))
             e=ddb.engine(output='raw',field_delimiter=self.kwargs['delimiter'])
             res=e.query("SELECT * FROM {0}".format(self.kwargs['table']))
             results=output_factory(res,output='raw',output_stream='STRING')
-            with open(FIFO) as fifo:
-                fifo.write(results)
+            
+            logging.debug("{0} grabbing data".format(thread_name))
+
+            with open(FIFO,"wr") as fifo:
+                # clear the buffer
+                try:
+                    res=fifo.readlines()
+                except:
+                    logging.debug("{0} nothing to read ".format(thread_name))            
+                    pass
+                try:
+    
+                    logging.debug("{0} writing".format(thread_name))
+                    data="\n".join(results.output)
+                    fifo.write(data)
+                except:
+                    logging.debug("{0} cant write".format(thread_name))            
+                    pass
         
         logging.debug("{0} finished".format(thread_name))
 
