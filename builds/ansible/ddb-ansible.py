a988e67809730cc5ce5f65b4a68d986006a47ff5
Charles Watkins <chris17453@gmail.com>
2019-05-13 09:24:14 -0400

    Bump Version 1.1.997

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -128,7 +128,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.1.996'
+__version__='1.1.997'
 
         
 # ############################################################################
@@ -389,7 +389,7 @@ sql_syntax = {
               'name': 'or',
               'optional': True,
               'parent': 'where'},
-             {'arguments':None,
+             {
               'data': False,#[{'sig': ['select']}],
               'name': 'union',
               'optional': True,
--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -42,7 +42,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.1.996'
+__version__='1.1.997'
 
         
 # ############################################################################
@@ -303,7 +303,7 @@ sql_syntax = {
               'name': 'or',
               'optional': True,
               'parent': 'where'},
-             {'arguments':None,
+             {
               'data': False,#[{'sig': ['select']}],
               'name': 'union',
               'optional': True,
