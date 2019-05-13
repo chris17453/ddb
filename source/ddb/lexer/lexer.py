cabcce7da1abb8999e95b4119439e6f41ffdd4a6
Charles Watkins <chris17453@gmail.com>
2019-05-13 09:37:29 -0400

    Bump Version 1.2.4

--- source/ddb/lexer/lexer.py
+++ source/ddb/lexer/lexer.py
@@ -401,7 +401,7 @@ class lexer:
             # This is where we exit if we reached the end of processing with a full length
             #print token_index,len(tokens)
             self.info("Switch Index: {0}, Token Index:{1}, Token Len: {2}".format(switch_index, token_index, len(tokens)))
-
+            self.info(tokens)
 
             self.info(curent_object)
             if token_index == len(tokens):
