851e9fbea130f9d0166a646cebde19454f0fac27
Charles Watkins <chris17453@gmail.com>
2019-05-14 12:45:27 -0400

    Bump Version 1.2.37

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -128,7 +128,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.36'
+__version__='1.2.37'
 
         
 # ############################################################################
@@ -3767,7 +3767,7 @@ def method_system_show_output_modules(context,query_object):
 # ############################################################################
 
 class lock:
-    max_lock_time=60
+    max_lock_time=2
     sleep_time=0.02
     LOCK_NONE=0
     LOCK_OWNER=1
