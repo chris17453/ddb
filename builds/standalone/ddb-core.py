c7ae6def1403e62bb14a9ee4fdc205f011bb9164
Charles Watkins <chris17453@gmail.com>
2019-05-15 11:55:15 -0400

    Bump Version 1.2.80

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ import logging
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.79'
+__version__='1.2.80'
 
         
 # ############################################################################
@@ -999,7 +999,7 @@ class lexer:
                 raise Exception(parsed['msg'])
             else:
                 self.query_objects.append(parsed['results'])
-        if None == self.query_objects:
+        if len(self.query_objects)==0:
             raise Exception("Invalid Syntax")
     def parse(self, tokens):
         highest_match=-1
