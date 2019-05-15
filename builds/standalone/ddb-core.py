6138c394344e4e5749487fe7640e8505f6bc3cfe
Charles Watkins <chris17453@gmail.com>
2019-05-15 11:27:08 -0400

    Bump Version 1.2.63

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ import logging
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.62'
+__version__='1.2.63'
 
         
 # ############################################################################
@@ -1075,7 +1075,7 @@ class lexer:
                 if 'depends_on' in segment:
                     depends_on = segment['depends_on']
                     if len(depends_on)>0:
-                        if depends_on[0]=='.'
+                        if depends_on[0]=='.':
                             depends_on_root=True
                             depends_on=depends_on[1:]
                     else:
