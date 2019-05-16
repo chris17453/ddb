4e5e368e8a28c0bf99d99704c2ad3f9627e2c1eb
Charles Watkins <chris17453@gmail.com>
2019-05-16 11:56:32 -0400

    Bump Version 1.2.184

--- source/test/svn_start.sh
+++ source/test/svn_start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+echo $(pwd)
+# clear old svn stuff
+rm source/test/svn_test/.svn -rf
+
+# start docker
 docker run -d  --name ddb-svn -p 80:80 krisdavison/svn-server:v3.0 
+
+sleep 3
+# pull empty repo
+cd source/test/svn_test
+svn co --no-auth-cache  --username user  --password password http://localhost/svn/SampleProject . --depth empty
+
+# add file
+cp ../MOCK_DATA.csv .
+svn add MOCK_DATA.csv  --no-auth-cache  --username user  --password password
+
