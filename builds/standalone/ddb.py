9fbe41e546346085850f75dbffc590cd8bdc62aa
Charles Watkins <chris17453@gmail.com>
2019-05-16 12:00:00 -0400

    Bump Version 1.2.188

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -41,7 +41,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.187'
+__version__='1.2.188'
 
         
 # ############################################################################
@@ -2500,8 +2500,8 @@ def enum(**enums):
 class engine:
     """A serverless flat file database engine"""
     def info(self,msg, arg1=None, arg2=None, arg3=None):
+        logging.info("PID:{0} : {2}, {3}, {4}".format(self.pid,msg,arg1,arg2))
         if True == self.debug:
-            logging.info("PID:{0} : {2}, {3}, {4}".format(self.pid,msg,arg1,arg2))
             if isinstance(arg1,str) :
                 print(msg, arg1, arg2, arg3)
             elif isinstance(arg1,object) :
