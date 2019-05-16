97dd140ac77dc183be52f79004892a439e236ef9
Charles Watkins <chris17453@gmail.com>
2019-05-16 08:42:43 -0400

    Bump Version 1.2.107

--- source/ddb/methods/table_create.py
+++ source/ddb/methods/table_create.py
@@ -57,9 +57,9 @@ def method_create_table(context, query_object):
                 repo_user=repo['user']
             if 'password' in repo:
                 repo_password=repo['password']
-            if 'dir' in repo:
+            if 'repo_dir' in repo:
                 repo_dir=repo['dir']
-            if 'file' in repo:
+            if 'repo_file' in repo:
                 repo_file=repo['file']
         else:
             repo_type=None
