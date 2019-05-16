a0aa7306494056855858be61400003c063d3548f
Charles Watkins <chris17453@gmail.com>
2019-05-16 08:30:17 -0400

    Bump Version 1.2.103

--- source/test/test.py
+++ source/test/test.py
@@ -101,7 +101,9 @@ class test_engine(unittest.TestCase):
             else:
                 file_name=os.path.join(self.basedir, self.temp_data)
 
-            results = engine.query("create table {0}('id','first_name','last_name','email','gender','ip_address') file='{1}' {2} data_starts_on=2".format(self.table_name, file_name,repo))
+            query="create table {0}('id','first_name','last_name','email','gender','ip_address') file='{1}' {2} data_starts_on=2".format(self.table_name, file_name,repo)
+            print query
+            results = engine.query(query)
             self.assertEqual(True, results.success)
             # test results length
             results = engine.query('select * from {0} LIMIT 10'.format(self.table_name))
