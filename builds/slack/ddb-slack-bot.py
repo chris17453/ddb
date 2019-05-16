bef59b395b284b8d5f1b31497da14bc1e097bdbf
Charles Watkins <chris17453@gmail.com>
2019-05-16 11:29:44 -0400

    Bump Version 1.2.178

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -42,7 +42,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.177'
+__version__='1.2.178'
 
         
 # ############################################################################
@@ -3654,6 +3654,9 @@ def method_system_commit(context):
                 else:
                     print ("deleting temp and copying to src",tmp['origin'],tmp['temp_source'])
                     swap_files(tmp['origin'],tmp['temp_source'],context.system['UUID'])
+                    if tmp['table'].data.repo_type=='svn':
+                       print ("svn thing")
+                       context.svn_commit_file(tmp['table'])
             context.internal['TEMP_FILES']={}
         else:
             raise Exception("Cannot commit, not in a transaction")
--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ import logging
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.177'
+__version__='1.2.178'
 
         
 # ############################################################################
@@ -3647,6 +3647,9 @@ def method_system_commit(context):
                 else:
                     print ("deleting temp and copying to src",tmp['origin'],tmp['temp_source'])
                     swap_files(tmp['origin'],tmp['temp_source'],context.system['UUID'])
+                    if tmp['table'].data.repo_type=='svn':
+                       print ("svn thing")
+                       context.svn_commit_file(tmp['table'])
             context.internal['TEMP_FILES']={}
         else:
             raise Exception("Cannot commit, not in a transaction")
