b77b9f0e4a5a974b36c7f0af9aa30be0f73aedd2
Charles Watkins <chris17453@gmail.com>
2019-05-16 12:53:23 -0400

    Bump Version 1.2.202

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ import logging
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.201'
+__version__='1.2.202'
 
         
 # ############################################################################
@@ -2767,7 +2767,8 @@ def process_line(context, query_object, line, line_number=0):
             else:
                 line_data = line_cleaned.split(table.delimiters.field,column_len)
                 cur_column_len = len(line_data)
-                if table.data.strict_columns and cur_column_len != column_len:
+                if table.data.strict_columns:
+                    if  cur_column_len != column_len:
                     if cur_column_len > column_len:
                         err = "Table {2}: Line #{0}, {1} extra Column(s)".format(line_number, cur_column_len - column_len, table.data.name)
                     else:
@@ -2778,6 +2779,10 @@ def process_line(context, query_object, line, line_number=0):
                     else:
                         line_data = None
                     line_type = context.data_type.ERROR
+                else:
+                    if  cur_column_len != column_len:
+                        for i in range(cur_column_len,column_len)
+                            line.data.append('')
                 if None != table.delimiters.block_quote:
                     line_data_cleaned = []
                     for d in line_data:
