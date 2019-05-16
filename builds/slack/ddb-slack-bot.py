df45a6213966568ab0b2721de17d15458e371506
Charles Watkins <chris17453@gmail.com>
2019-05-16 11:16:41 -0400

    Bump Version 1.2.167

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -42,7 +42,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.166'
+__version__='1.2.167'
 
         
 # ############################################################################
@@ -3651,9 +3651,9 @@ def method_system_commit(context):
                     lock.release(table_key)
                 else:
                     swap_files(tmp['origin'],tmp['temp_source'],context.system['UUID'])
-            context.internal['TEMP_FILES']={}
             if tmp['table'].data.repo=='svn':
                 context.svn_commit_file(tmp['table'])
+            context.internal['TEMP_FILES']={}
         else:
             raise Exception("Cannot commit, not in a transaction")
         return query_results(success=True)
