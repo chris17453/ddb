e9bccdf6e06139b99060a20791b9ec05dd7c5159
Charles Watkins <chris17453@gmail.com>
2019-05-16 09:44:20 -0400

    Bump Version 1.2.119

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -42,7 +42,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.118'
+__version__='1.2.119'
 
         
 # ############################################################################
@@ -991,7 +991,7 @@ language={'commands': [{'name': 'show columns',
 class lexer:
     def __init__(self, query, debug=False):
         self.keep_non_keywords=True
-        self.debug = True
+        self.debug = debug
         self.query_objects = []
         if  query==None:
             raise Exception("Invalid Syntax")
