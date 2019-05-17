d92aa99cb42baca0aa6757e599f566b290c43c51
Charles Watkins <chris17453@gmail.com>
2019-05-17 09:58:06 -0400

    Bump Version 1.2.203

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -42,7 +42,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.202'
+__version__='1.2.203'
 
         
 # ############################################################################
@@ -2774,21 +2774,21 @@ def process_line(context, query_object, line, line_number=0):
             else:
                 line_data = line_cleaned.split(table.delimiters.field,column_len)
                 cur_column_len = len(line_data)
-                if table.data.strict_columns:
+                if table.data.strict_columns==True:
                     if  cur_column_len != column_len:
-                    if cur_column_len > column_len:
-                        err = "Table {2}: Line #{0}, {1} extra Column(s)".format(line_number, cur_column_len - column_len, table.data.name)
-                    else:
-                        err = "Table {2}: Line #{0}, missing {1} Column(s)".format(line_number, column_len - cur_column_len, table.data.name)
-                    line_type = context.data_type.ERROR
-                    if True == table.visible.errors:
-                        line_data = line_cleaned
-                    else:
-                        line_data = None
-                    line_type = context.data_type.ERROR
+                        if cur_column_len > column_len:
+                            err = "Table {2}: Line #{0}, {1} extra Column(s)".format(line_number, cur_column_len - column_len, table.data.name)
+                        else:
+                            err = "Table {2}: Line #{0}, missing {1} Column(s)".format(line_number, column_len - cur_column_len, table.data.name)
+                        line_type = context.data_type.ERROR
+                        if True == table.visible.errors:
+                            line_data = line_cleaned
+                        else:
+                            line_data = None
+                        line_type = context.data_type.ERROR
                 else:
                     if  cur_column_len != column_len:
-                        for i in range(cur_column_len,column_len)
+                        for i in range(cur_column_len,column_len):
                             line.data.append('')
                 if None != table.delimiters.block_quote:
                     line_data_cleaned = []
