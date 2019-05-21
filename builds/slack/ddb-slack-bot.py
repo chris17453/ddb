9c1bcc5d47775bd1e5d56802ba2458e053869467
Charles Watkins <charles@titandws.com>
2019-05-20 22:25:03 -0400

    Bump Version 1.2.266

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -42,7 +42,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.265'
+__version__='1.2.266'
 
         
 # ############################################################################
@@ -1371,12 +1371,13 @@ class lexer:
             temp_haystacks = haystacks
         index = 0
         for needle in temp_needles:
-            print needle[0]
+            first_char=needle[0]
+            last_char=needle[-1]
             if index >= len(temp_haystacks):
                 return False
             haystack = temp_haystacks[index]
-            if needle[0]!='$':
-                if (needle[0] != '{' and needle[-1] != '}'):
+            if first_char!='$':
+                if first_char != '{' and last_char != '}'):
                     if needle.lower() != haystack.lower():
                         return False
             index += 1
