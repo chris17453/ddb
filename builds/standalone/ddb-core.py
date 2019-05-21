673a8eafb3b0d08f550b49bf5dcf212e198c003c
Charles Watkins <charles@titandws.com>
2019-05-21 15:28:42 -0400

    Bump Version 1.2.283

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ import logging
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.282'
+__version__='1.2.283'
 
         
 # ############################################################################
@@ -305,7 +305,6 @@ language={'commands': [{'name': 'show columns',
                              'name': 'where',
                              'optional': True,
                              'store_array': True},
-                            {'arguments': 0,
                             {'data': [{'sig': ['and','{e1}','$operators:c','{e2}'] } ] ,
                              'depends_on': 'where',
                              'jump': 'where',
