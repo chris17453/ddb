2e9f51d462f527348cd551cae82f7ade1c635832
Charles Watkins <charles@titandws.com>
2019-05-21 06:44:03 -0400

    Bump Version 1.2.269

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -42,7 +42,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.268'
+__version__='1.2.269'
 
         
 # ############################################################################
@@ -989,7 +989,7 @@ class lexer:
         last_char=word[-1]
         if first_char == '{' and last_char == '}':
                 definition='single'
-        elif if first_char == '$'
+        elif first_char == '$'
             definition='internal'
         else:
             definition=None       
