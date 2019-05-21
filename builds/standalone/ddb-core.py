b8d01bfe32afc08d13f664790b5ed6b6f1c19cde
Charles Watkins <charles@titandws.com>
2019-05-21 06:51:27 -0400

    Bump Version 1.2.274

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ import logging
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.273'
+__version__='1.2.274'
 
         
 # ############################################################################
@@ -1018,7 +1018,7 @@ class lexer:
         elif definition=='internal':
             variable=word[1:]
             index_of_colon=variable.find(':')
-            if index_of_colon==-1
+            if index_of_colon==-1:
                 key=variable[index_of_colon+1:]
             else:
                 key=variable
