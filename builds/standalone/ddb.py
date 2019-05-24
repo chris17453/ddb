7851d8df6f99cad37747628c6d7c67a6f2b70aa6
Charles Watkins <charles@titandws.com>
2019-05-23 22:52:21 -0400

    Bump Version 1.2.311

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.310'
+__version__='1.2.311'
 
         
 # ############################################################################
@@ -2443,6 +2443,114 @@ class engine:
 # File   : ./source/ddb/methods/record_core.py
 # ############################################################################
 
+class match():
+    def evaluate_single_match(self,context,test, row, table):
+        compare1 = None
+        compare2 = None
+        compare1_is_column = False
+        compare2_is_column = False
+        comparitor = test['c']
+        for column in table.columns:
+            if column.data.name == test['e1']:
+                index = table.ordinals[column.data.name]
+                compare1 = row[index]  # table.ordinals[].get_data_from_column(column,row)
+                compare1_is_column = True
+            elif column.data.name == test['e2']:
+                index = table.ordinals[column.data.name]
+                compare2 = row[index]  # table.get_data_from_column(column,row)
+                compare2_is_column = True
+            if None != compare1 and None != compare2:
+                break
+        if not compare1_is_column and not compare2_is_column:
+            raise Exception("expression invalid {0}".format(test))
+        if None == compare1:
+            compare1 = test['e1']
+        if None == compare2:
+            compare2 = test['e2']
+        if comparitor == '=' or comparitor == 'is':
+            if compare1 == compare2:
+                return True
+        elif comparitor == 'like':  # paritial match
+            if True == compare1_is_column and True == compare2_is_column:
+                raise Exception("Where invalid {0}, like cant be between 2 columns".format(test))
+            if True == compare1_is_column:
+                like = compare2
+                data = compare1
+            else:
+                like = compare1
+                data = compare2
+            if None == like:
+                return False
+            if like[0] == '%':
+                like_left = True
+            else:
+                like_left = False
+            if like[-1] == '%':
+                like_right = True
+            else:
+                like_right = False
+            if True == like_right and True == like_left:
+                if data.find(like[1:-1]) > -1:
+                    return True
+                else:
+                    return False
+            if True == like_left:
+                if data[-(len(like) - 1):] == like[1:]:
+                    return True
+                else:
+                    return False
+            if True == like_right:
+                if data[0:(len(like) - 1)] == like[0:-1]:
+                    return True
+                else:
+                    return False
+            return False
+        elif comparitor == '<':
+            if compare1 < compare2:
+                return True
+        elif comparitor == '>':
+            if compare1 > compare2:
+                return True
+        elif comparitor == '>=':
+            if compare1 >= compare2:
+                return True
+        elif comparitor == '<=':
+            if compare1 <= compare2:
+                return True
+        elif comparitor == '!=' or comparitor == '<>' or comparitor == 'not':
+            if compare1 != compare2:
+                return True
+        return False
+    def evaluate_match(self,context,query_object, row):
+        table=query_object['table']
+        where=query_object['meta']['where']
+        if None == row:
+            return False
+        success = None
+        skip_section = False
+        operation = ""
+        for test in where:
+            if 'and' in test and skip_section:
+                continue
+            else:
+                skip_section = False
+            operation = None
+            if 'where' in test:
+                operation = 'where'
+            elif 'or' in test:
+                operation = 'or'
+                if success:
+                    return True
+            elif 'and' in test:
+                operation = 'and'
+                if not success:
+                    skip_section = True
+                    continue
+            test_operation = test[operation]
+            success = self.evaluate_single_match(context,test_operation, row, table)
+        if success is None:
+            return False
+        return success
 def process_line(context, query_object, line, line_number=0,column_count=0,delimiter=',',visible_whitespace=None,visible_comments=None, visible_errors=None):
     err = None
     table=query_object['table']
@@ -2496,7 +2604,7 @@ def process_line(context, query_object, line, line_number=0,column_count=0,delim
             match_results = True
         else:
             if line_type == context.data_type.DATA:
-                match_results = context.match.evaluate_match(context,query_object, line_data)
+                match_results = match().evaluate_match(context,query_object, line_data)
             else:
                 match_results = False
         if visible_whitespace is False and line_type==context.data_type.WHITESPACE:
