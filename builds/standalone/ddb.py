0bcfff9225649ad2330d24408c7780727ddb13d8
Charles Watkins <charles@titandws.com>
2019-05-23 23:08:06 -0400

    EOD Thursday. 1 million rows in 8 seconds

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.311'
+__version__='1.2.312'
 
         
 # ############################################################################
@@ -2560,11 +2560,11 @@ def process_line(context, query_object, line, line_number=0,column_count=0,delim
     if table.data.starts_on_line > line_number:
         line_type = context.data_type.COMMENT
         line_data = line
-        match=False
+        try_match=False
     else:
         line_type = context.data_type.DATA
-        match=True
-    if match:
+        try_match=True
+    if try_match:
         if not line_cleaned:
             if True == visible_whitespace:
                 line_data = ['']
