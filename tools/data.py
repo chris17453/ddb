9dce11bd88c2c4df499cf5075a34a1d4fa488773
Charles Watkins <charles@titandws.com>
2019-06-05 19:38:03 -0400

    Bump Version 1.2.397

--- tools/data.py
+++ tools/data.py
@@ -252,7 +252,7 @@ def init(command,classes,class_spec):
                 if '_arguments' in classes[_class]  or class_spec[_class]['storage']=='array':
                         #print ("            print(so)")
                         if classes[_class][variable]['storage']=='single' or class_spec[_class]['type']=='single':
-                            print ("            if gv(so,['meta','{0}']):".format(_class)
+                            print ("            if gv(so,['meta','{0}']):".format(_class))
                         else:
                             print ("            if gv(so,['meta','{0}','{1}']):".format(command_name,_class,",".join(var)))
                         print ("                self.{1:<20}=[]".format(command_name,_class.replace(" ","_")))
