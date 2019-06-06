dada42b8201d21a50ea7ab0ea236d699545bcade
Charles Watkins <charles@titandws.com>
2019-06-05 20:24:23 -0400

    Bump Version 1.2.402

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.401'
+__version__='1.2.402'
 
         
 # ############################################################################
@@ -1330,6 +1330,9 @@ class tokenizer:
 # ############################################################################
 
 def gv(o,keys):
+    print ("GV")
+    print keys
+    print o
     if o:
         if isinstance(keys,str):
             if keys in o:
@@ -1338,12 +1341,15 @@ def gv(o,keys):
                 return None
         else:
             for key in keys:
+                print ("->{0}".format(key))
                 if key in o:
+                    print ("Got It")
                     o=o[key]
                 else:
                     return None
     else:     
         return None
+    print (o)
     return o
 class show_columns:
     __slots__=()
--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.401'
+__version__='1.2.402'
 
         
 # ############################################################################
@@ -1244,6 +1244,9 @@ class tokenizer:
 # ############################################################################
 
 def gv(o,keys):
+    print ("GV")
+    print keys
+    print o
     if o:
         if isinstance(keys,str):
             if keys in o:
@@ -1252,12 +1255,15 @@ def gv(o,keys):
                 return None
         else:
             for key in keys:
+                print ("->{0}".format(key))
                 if key in o:
+                    print ("Got It")
                     o=o[key]
                 else:
                     return None
     else:     
         return None
+    print (o)
     return o
 class show_columns:
     __slots__=()
