b2923320a17418f0f390d2ab3c92c1ee84469a05
Watkins, Charles <charles.watkins@etrade.com>
2019-06-07 12:41:00 -0400

    Bump Version 1.2.519

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ from subprocess import Popen,PIPE
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.518'
+__version__='1.2.519'
 
         
 # ############################################################################
@@ -3255,6 +3255,115 @@ def create_single(context, query_object, temp_file, requires_new_line):
 # File   : ./source/ddb/methods/record_select.py
 # ############################################################################
 
+class match2:
+    def evaluate_single_match(self,test, row, table):
+        compare1 = None
+        compare2 = None
+        compare1_is_column = False
+        compare2_is_column = False
+        comparitor = test['c']
+        for column in table.columns:
+            if column.data.name == test.e1:
+                index = table.ordinals[column.data.name]
+                compare1 = row[index]  # table.ordinals[].get_data_from_column(column,row)
+                compare1_is_column = True
+            elif column.data.name == test.e2:
+                index = table.ordinals[column.data.name]
+                compare2 = row[index]  # table.get_data_from_column(column,row)
+                compare2_is_column = True
+            if None != compare1 and None != compare2:
+                break
+        if not compare1_is_column and not compare2_is_column:
+            raise Exception("expression invalid {0}".format(test))
+        if None == compare1:
+            compare1 = test.e1
+        if None == compare2:
+            compare2 = test.e2
+        if comparitor == '=' or comparitor == 'is':
+            if compare1 == compare2:
+                return True
+        elif comparitor == 'like':  # paritial match
+            if True == compare1_is_column and True == compare2_is_column:
+                raise Exception("Where invalid {0}, like cant be between 2 columns".format(test))
+            if True == compare1_is_column:
+                like = compare2
+                data = compare1
+            else:
+                like = compare1
+                data = compare2
+            if None == like:
+                return False
+            if like[0] == '%':
+                like_left = True
+            else:
+                like_left = False
+            if like[-1] == '%':
+                like_right = True
+            else:
+                like_right = False
+            if True == like_right and True == like_left:
+                if data.find(like[1:-1]) > -1:
+                    return True
+                else:
+                    return False
+            if True == like_left:
+                if data[-(len(like) - 1):] == like[1:]:
+                    return True
+                else:
+                    return False
+            if True == like_right:
+                if data[0:(len(like) - 1)] == like[0:-1]:
+                    return True
+                else:
+                    return False
+            return False
+        elif comparitor == '<':
+            if compare1 < compare2:
+                return True
+        elif comparitor == '>':
+            if compare1 > compare2:
+                return True
+        elif comparitor == '>=':
+            if compare1 >= compare2:
+                return True
+        elif comparitor == '<=':
+            if compare1 <= compare2:
+                return True
+        elif comparitor == '!=' or comparitor == '<>' or comparitor == 'not':
+            if compare1 != compare2:
+                return True
+        return False
+    def evaluate_match(self,context,meta, row):
+        if None == row:
+            return False
+        table=meta.table
+        where=meta.where
+        success = None
+        skip_section = False
+        operation = ""
+        for test in where:
+            test.condition=test.condition.lower()
+            if test.condition=='and' and skip_section:
+                continue
+            else:
+                skip_section = False
+            operation = None
+            if test.condition=='where':
+                operation = 'where'
+            elif test.condition=='or':
+                operation = 'or'
+                if success:
+                    return True
+            elif test.condition=='and':
+                operation = 'and'
+                if not success:
+                    skip_section = True
+                    continue
+            test_operation = test[operation]
+            success = self.evaluate_single_match(test_operation, row, table)
+        if success is None:
+            return False
+        return success
 context_sort=[]
 def method_select(context, meta, parser):
         context.info(meta)
@@ -3615,115 +3724,7 @@ def process_line3(context,meta, line, line_number=0,column_count=0,delimiter=','
             'line_number': line_number, 
             'match': match_results, 
             'error': err}
-class match2:
-    def evaluate_single_match(self,test, row, table):
-        compare1 = None
-        compare2 = None
-        compare1_is_column = False
-        compare2_is_column = False
-        comparitor = test['c']
-        for column in table.columns:
-            if column.data.name == test.e1:
-                index = table.ordinals[column.data.name]
-                compare1 = row[index]  # table.ordinals[].get_data_from_column(column,row)
-                compare1_is_column = True
-            elif column.data.name == test.e2:
-                index = table.ordinals[column.data.name]
-                compare2 = row[index]  # table.get_data_from_column(column,row)
-                compare2_is_column = True
-            if None != compare1 and None != compare2:
-                break
-        if not compare1_is_column and not compare2_is_column:
-            raise Exception("expression invalid {0}".format(test))
-        if None == compare1:
-            compare1 = test.e1
-        if None == compare2:
-            compare2 = test.e2
-        if comparitor == '=' or comparitor == 'is':
-            if compare1 == compare2:
-                return True
-        elif comparitor == 'like':  # paritial match
-            if True == compare1_is_column and True == compare2_is_column:
-                raise Exception("Where invalid {0}, like cant be between 2 columns".format(test))
-            if True == compare1_is_column:
-                like = compare2
-                data = compare1
-            else:
-                like = compare1
-                data = compare2
-            if None == like:
-                return False
-            if like[0] == '%':
-                like_left = True
-            else:
-                like_left = False
-            if like[-1] == '%':
-                like_right = True
-            else:
-                like_right = False
-            if True == like_right and True == like_left:
-                if data.find(like[1:-1]) > -1:
-                    return True
-                else:
-                    return False
-            if True == like_left:
-                if data[-(len(like) - 1):] == like[1:]:
-                    return True
-                else:
-                    return False
-            if True == like_right:
-                if data[0:(len(like) - 1)] == like[0:-1]:
-                    return True
-                else:
-                    return False
-            return False
-        elif comparitor == '<':
-            if compare1 < compare2:
-                return True
-        elif comparitor == '>':
-            if compare1 > compare2:
-                return True
-        elif comparitor == '>=':
-            if compare1 >= compare2:
-                return True
-        elif comparitor == '<=':
-            if compare1 <= compare2:
-                return True
-        elif comparitor == '!=' or comparitor == '<>' or comparitor == 'not':
-            if compare1 != compare2:
-                return True
-        return False
-    def evaluate_match(self,context,meta, row):
-        if None == row:
-            return False
-        table=meta.table
-        where=meta.where
-        success = None
-        skip_section = False
-        operation = ""
-        for test in where:
-            test.condition=test.condition.lower()
-            if test.condition=='and' and skip_section:
-                continue
-            else:
-                skip_section = False
-            operation = None
-            if test.condition=='where':
-                operation = 'where'
-            elif test.condition=='or':
-                operation = 'or'
-                if success:
-                    return True
-            elif test.condition=='and':
-                operation = 'and'
-                if not success:
-                    skip_section = True
-                    continue
-            test_operation = test[operation]
-            success = self.evaluate_single_match(test_operation, row, table)
-        if success is None:
-            return False
-        return success
+
         
 # ############################################################################
 # Module : methods-records-update
