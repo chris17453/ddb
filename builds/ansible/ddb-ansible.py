e46e98fbfe43ba38289a371e71c4290a1c5557f1
Charles Watkins <charles@titandws.com>
2019-06-07 00:20:05 -0400

    Bump Version 1.2.451

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.450'
+__version__='1.2.451'
 
         
 # ############################################################################
@@ -1350,7 +1350,7 @@ class debugger:
                 if len(value)==1:
                     print ("{0} - {1} :{2}".format(pad,var,value))
                 else:
-                    print ("{0} - {1} :".format(pad,name))
+                    print ("{0} - {1} :".format(pad,var))
                     for item in value:
                         debugger(item,var,depth+3)
             elif callable(value):
