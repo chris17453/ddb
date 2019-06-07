9fee2ccc7e49cc0a8dd1e6feddcbbd3dea5c083d
Charles Watkins <charles@titandws.com>
2019-06-07 00:18:10 -0400

    Bump Version 1.2.449

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.448'
+__version__='1.2.449'
 
         
 # ############################################################################
@@ -1336,9 +1336,6 @@ class debugger:
             pad+=' '
         if depth==0:
             print ("Debug:")
-        else:
-            print ("{0}->{1}".format(pad,name))
-            pad+=" "
         variables = [i for i in dir(obj) if not i.startswith('__')]
         empty=[]
         for var in variables:
@@ -1350,8 +1347,12 @@ class debugger:
             if  isinstance(value,float):
                 print("{2}{0:<20}{1}".format(var+':',value,pad))
             elif isinstance(value,list):
-                for item in value:
-                    debugger(item,var,depth+1)
+                if len(value)==1:
+                    print ("{0} - {1} :{2}".format(pad,name,value))
+                else:
+                    print ("{0} - {1} :".format(pad,name))
+                    for item in value:
+                        debugger(item,var,depth+1)
             elif callable(value):
                 continue
             if value==None:
