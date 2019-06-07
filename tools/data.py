438b49a00e086b24199da28b57f80aa64c8742f6
Charles Watkins <charles@titandws.com>
2019-06-06 23:57:10 -0400

    Bump Version 1.2.440

--- tools/data.py
+++ tools/data.py
@@ -352,10 +352,12 @@ class debugger:
             elif isinstance(value,list):
                 for item in value:
                     debugger(item,depth+1)
-            elif value!=None:
+            elif False==callable(value):
                 print("{2}{0:<20}{1}".format(var+':','class',pad))
                 #debugger(value,depth+1)
             else:
+                if callable(value):
+                    continue
                 empty.append(var)
         if len(empty)>0:
             print ("{1}Empty Vars: {0}".format(",".join(empty),pad))
