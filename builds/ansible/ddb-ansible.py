76f6c0838a69e982682dea275b22f466bac0b5df
Watkins, Charles <charles.watkins@etrade.com>
2019-06-07 12:48:07 -0400

    Bump Version 1.2.523

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.522'
+__version__='1.2.523'
 
         
 # ############################################################################
@@ -3453,7 +3453,12 @@ class match2:
                 if not success:
                     skip_section = True
                     continue
-            test_operation = test[operation]
+            if hasattr( test,'where'):
+                test_operation = test.where
+            elif hasattr( test,'and'):
+                test_operation = test.and
+            elif hasattr( test,'or'):
+                test_operation = test.or
             success = self.evaluate_single_match(test_operation, row, table)
         if success is None:
             return False
@@ -3562,7 +3567,6 @@ def select_has_functions(context,meta):
 def add_table_columns(context,meta,temp_table):
     for column in meta.columns:
         display = None
-        print meta.columns
         if column.display:
             display = column.display
             context.info("RENAME COLUMN", display)
--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.522'
+__version__='1.2.523'
 
         
 # ############################################################################
@@ -3367,7 +3367,12 @@ class match2:
                 if not success:
                     skip_section = True
                     continue
-            test_operation = test[operation]
+            if hasattr( test,'where'):
+                test_operation = test.where
+            elif hasattr( test,'and'):
+                test_operation = test.and
+            elif hasattr( test,'or'):
+                test_operation = test.or
             success = self.evaluate_single_match(test_operation, row, table)
         if success is None:
             return False
@@ -3476,7 +3481,6 @@ def select_has_functions(context,meta):
 def add_table_columns(context,meta,temp_table):
     for column in meta.columns:
         display = None
-        print meta.columns
         if column.display:
             display = column.display
             context.info("RENAME COLUMN", display)
