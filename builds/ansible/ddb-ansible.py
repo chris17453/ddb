b41c2cbe44a321bed951ba4e9cf5c97870c67ef3
Charles Watkins <charles@titandws.com>
2019-06-06 23:37:13 -0400

    Bump Version 1.2.428

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.427'
+__version__='1.2.428'
 
         
 # ############################################################################
@@ -1336,8 +1336,8 @@ class debugger:
         empty=[]
         for var in variables:
             value=getattr(obj,var)
-            if isinstance(value, (type, types.ClassType)):
-                print ("CLASS")
+            if  hasattr(value,'__class__')
+                print("{0:<20}{1}".format(var+':',"CLASS"))
             elif value!=None:
                 print("{0:<20}{1}".format(var+':',value))
             else:
