5bcc206bacf8e0499c884ab7799d899d876a096d
Charles Watkins <charles@titandws.com>
2019-06-07 00:26:35 -0400

    Bump Version 1.2.452

--- tools/data.py
+++ tools/data.py
@@ -350,19 +350,19 @@ class debugger:
         for var in variables:
             value=getattr(obj,var)
             if  isinstance(value,str):
-                print("{2} {0}{1}".format(var+':',value,pad))
+                print("{2}{0} {1}".format(var+':',value,pad))
             if  isinstance(value,int):
-                print("{2} {0}{1}".format(var+':',value,pad))
+                print("{2}{0} {1}".format(var+':',value,pad))
             if  isinstance(value,float):
-                print("{2} {0}{1}".format(var+':',value,pad))
+                print("{2}{0} {1}".format(var+':',value,pad))
             elif isinstance(value,list):
                 
                 if len(value)==1:
-                    print ("{0} - {1} :{2}".format(pad,var,value))
+                    print ("{0}- {1} :{2}".format(pad,var,value))
                 else:
-                    print ("{0} - {1} :".format(pad,var))
+                    print ("{0}- {1} :".format(pad,var))
                     for item in value:
-                        debugger(item,var,depth+3)
+                        debugger(item,var,depth+2)
             elif callable(value):
                 continue
             if value==None:
