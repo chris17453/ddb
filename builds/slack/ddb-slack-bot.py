5ae51dfdee497bc5abf047b07ac7d6d9a69a7eec
Watkins, Charles <charles.watkins@etrade.com>
2019-06-07 11:54:55 -0400

    Bump Version 1.2.506

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.505'
+__version__='1.2.506'
 
         
 # ############################################################################
@@ -3512,7 +3512,7 @@ def sort_cmp( x, y):
         else:
             return 1 * direction
     return 0
-def limit(context, query_object, data):
+def limit(context, meta, data):
     index = 0
     length = None
     if meta.limit:
