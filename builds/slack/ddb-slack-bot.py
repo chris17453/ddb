90cfa3ee1b8e4dedd96b44bc2d083cd26f7eb254
Watkins, Charles <charles.watkins@etrade.com>
2019-06-24 13:02:11 -0400

    Bump Version 1.2.540

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.539'
+__version__='1.2.540'
 
         
 # ############################################################################
@@ -67,9 +67,10 @@ language={'commands': [{'name': 'show columns',
                'segments': [{'data': [{'signature': ['show', 'variables']}],
                              'name': 'show variables'}]},
               {'name': 'select',
-               'segments': [{'data': [{'signature': ['select'],'vars':{'table':None }}],
+              'vars':{'table':None },
+               'segments': [{'data': [{'signature': ['select']}],
                              'name': 'select',
-                             'vars':{'table':None },
+                             ,
                              'optional': False},
                             {'data': [{'signature': ['distinct']}],
                              'name': 'distinct',
@@ -1402,6 +1403,7 @@ class select:
     group_by             = None        # optional [ group by() ]
     source               = None        # optional source()
     limit                = None        # optional limit()
+    table                = None
     where                = None        # optional [ where() ]
     columns              = []          #          columns()
     order_by             = None        # optional [ order by() ]
@@ -1414,6 +1416,7 @@ class select:
                 self.source= self._source(table = gv(so,['meta','source','table']),display = gv(so,['meta','source','display']),database = gv(so,['meta','source','database']))
             if gv(so,['meta','limit']):
                 self.limit= self._limit(start = gv(so,['meta','limit','start']),length = gv(so,['meta','limit','length']))
+            self.table = gv(so,['meta','select','table'])
             if gv(so,['meta','where']):
                 self.where=[]
                 for item in gv(so,['meta','where']):
