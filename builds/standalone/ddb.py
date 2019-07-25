2b40bc596694e2f6c9f7570540c3a70ecaa43d3f
Charles Watkins <charles@titandws.com>
2019-07-24 23:10:37 -0400

    Bump Version 1.2.610

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.609'
+__version__='1.2.610'
 
         
 # ############################################################################
@@ -3325,7 +3325,7 @@ class query_results:
 # File   : ./source/ddb/methods/record_delete.py
 # ############################################################################
 
-def method_delete(context, query_object):
+def method_delete(context, meta):
     try:
         meta.table=get_table(context,meta)
         line_number = 1
