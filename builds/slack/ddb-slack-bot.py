2206a7f25f86157e0426638d95a770f8abc1fbae
Charles Watkins <charles@titandws.com>
2019-07-24 23:14:09 -0400

    Bump Version 1.2.617

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.616'
+__version__='1.2.617'
 
         
 # ############################################################################
@@ -3831,7 +3831,7 @@ def method_upsert(context, meta):
                         context.add_error(processed_line.error)
                     line_number += 1
                     if True == processed_line.match:
-                        results = update_single(context,query_object, temp_file,  False, processed_line)
+                        results = update_single(context,meta, temp_file,  False, processed_line)
                         if True == results.success:
                             diff.append(results.line)
                             affected_rows += 1
