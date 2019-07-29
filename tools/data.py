c862dd87b15468b027080957dc9b1cd68d3fea58
Charles Watkins <charles@titandws.com>
2019-07-29 07:54:26 -0400

    Bump Version 1.2.696

--- tools/data.py
+++ tools/data.py
@@ -35,7 +35,7 @@ def get_data(command,classes,class_spec):
                 storage='array'
         no_keyword=None
         if 'no_keyword' in segment:
-                no_keyword=True
+                no_keyword=True 
 
         if no_keyword:
              seg_type='single'
@@ -287,7 +287,7 @@ def init(command,classes,class_spec):
                         print ("                self.{0}=[]".format(_class.replace(" ","_")))
                         print ("                for item in gv(so,['meta','{0}']):".format(_class))
                         print ("                    instance_type=safe_name(item.keys()[0])")
-                        #print ("                    print('*'+instance_type+'*')")
+                        print ("                    print('*'+instance_type+'*')")
                         print ("                    self.{1}.append( type('_'+instance_type,(),{{ {2} }}) )".format(command_name,_class.replace(" ","_"),",".join(var_dict)))
                 else:
                     if class_spec[_class]['parent']==None:
