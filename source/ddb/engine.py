db8d5a1ce3bfc35fdbc467e06dc51905796a9a16
Charles Watkins <charles@titandws.com>
2019-07-30 20:23:50 -0400

    Bump Version 1.2.747

--- source/ddb/engine.py
+++ source/ddb/engine.py
@@ -228,7 +228,7 @@ class engine:
                 self.results = method_update(self,meta_class)
 
             elif mode == 'upsert' and self.internal['READONLY']==None:
-                self.results = method_upsert(self,meta_class,query_object)
+                self.results = method_upsert(self,meta_class,query_object,meta)
             
             elif mode == 'delete' and self.internal['READONLY']==None:
                 self.results = method_delete(self,meta_class)
