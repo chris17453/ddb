048b366347abb287e9e3a2f8313dab7337244461
Charles Watkins <charles@titandws.com>
2019-08-04 19:07:04 -0400

    Bump Version 1.2.840

--- source/ddb/engine.py
+++ source/ddb/engine.py
@@ -96,6 +96,7 @@ class engine:
         self.system['DEBUG']=False
         self.system['AUTOCOMMIT']=True
         self.system['OUTPUT_MODULE']=output
+        self.system['DATA_DIRECTORY']=config_dir
         self.system['VERSION']=__version__
         
         try:
