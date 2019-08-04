8517c46655b62951fa4cab537f0dccd025ecaa12
Charles Watkins <charles@titandws.com>
2019-08-04 18:16:03 -0400

    Bump Version 1.2.829

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.828'
+__version__='1.2.829'
 
         
 # ############################################################################
@@ -2537,7 +2537,7 @@ class database:
                     err="Data file does not exist. {0}".format(normalize_path(data_file))
                     raise Exception(err)
         if not temporary:
-            if None == self.config_file:
+            if None == self.config_dir:
                 raise Exception("Not using a config file")
             config_directory = os.path.dirname(self.config_file)
         else:
