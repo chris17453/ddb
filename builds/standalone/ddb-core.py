626b6c8b6bfd1a22b4cb9499cb5cad5e971c2500
Charles Watkins <charles@titandws.com>
2019-08-06 21:00:35 -0400

    Bump Version 1.2.876

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ from subprocess import Popen,PIPE
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.875'
+__version__='1.2.876'
 
         
 # ############################################################################
@@ -2525,9 +2525,11 @@ class database:
             raise Exception("table already exists")
         if repo:
             if repo.protocol!='svn':
+                protocol_svn='svn'
                 for a in repo.protocol:
                     print ord(a)
-                for a in 'svn':
+                print "="
+                for a in protocol_svn:
                     print ord(a)
                 abs_data_file=normalize_path(data_file)
                 print "??",repo.protocol
