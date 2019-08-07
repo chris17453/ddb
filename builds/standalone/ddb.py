0d7bd312bef8cc47c7391ee02cc026c03afec5b7
Charles Watkins <charles@titandws.com>
2019-08-06 20:37:45 -0400

    Bump Version 1.2.871

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.870'
+__version__='1.2.871'
 
         
 # ############################################################################
@@ -1148,12 +1148,14 @@ class tokenizer:
                         self.info("IN BLOCK", c)
                         break
                 else:
-                    if (fragment== block[1] and fragment is not None) or c >= text_length - 1:
-                        just_crossed_block = True
-                        self.info("NOT IN BLOCK", c)
-                        in_block = None
-                        c += delimter_len
-                        break
+                    if block:
+                        if (fragment== block[1] and fragment is not None) or c >= text_length - 1:
+                            just_crossed_block = True
+                            self.info("NOT IN BLOCK", c)
+                            in_block = None
+                            block=None
+                            c += delimter_len
+                            break
             if in_block  is not None:
                 self.info("in block skip")
                 if not just_crossed_block:
