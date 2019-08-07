168c0fe3aae4572950c4d30d462a63505990d3ba
Charles Watkins <charles@titandws.com>
2019-08-06 20:58:17 -0400

    Bump Version 1.2.875

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.874'
+__version__='1.2.875'
 
         
 # ############################################################################
@@ -2619,6 +2619,10 @@ class database:
             raise Exception("table already exists")
         if repo:
             if repo.protocol!='svn':
+                for a in repo.protocol:
+                    print ord(a)
+                for a in 'svn':
+                    print ord(a)
                 abs_data_file=normalize_path(data_file)
                 print "??",repo.protocol
                 print "??",data_file,abs_data_file
