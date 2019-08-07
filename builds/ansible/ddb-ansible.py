c6900345d18b067c1926b99e8e56497370f2742b
Charles Watkins <charles@titandws.com>
2019-08-06 20:10:20 -0400

    Bump Version 1.2.859

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.858'
+__version__='1.2.859'
 
         
 # ############################################################################
@@ -1234,12 +1234,14 @@ class tokenizer:
                         c += delimter_len
                         self.info("IN BLOCK", c)
                         break
-                if (fragment== block[1] and fragment is not None) or c >= text_length - 1:
-                    just_crossed_block = True
-                    self.info("NOT IN BLOCK", c)
-                    in_block = None
-                    c += delimter_len
-                    break
+                if block:
+                    if (fragment== block[1] and fragment is not None) or c >= text_length - 1:
+                        just_crossed_block = True
+                        self.info("NOT IN BLOCK", c)
+                        in_block = None
+                        block=None
+                        c += delimter_len
+                        break
             if in_block  is not None:
                 self.info("in block skip")
                 if not just_crossed_block:
