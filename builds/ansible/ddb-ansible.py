5d5c5a5ab0c0ac5d5cb75e9a6c3014ecf5780b34
Charles Watkins <charles@titandws.com>
2019-08-06 23:32:23 -0400

    Bump Version 1.2.914

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.913'
+__version__='1.2.914'
 
         
 # ############################################################################
@@ -1246,7 +1246,7 @@ class tokenizer:
                         else:
                             if delimiter in whitespace:
                                 delimiter_type = 'whitespace'
-                        if True == discard_whitespace and word in whitespace:
+                        if True == discard_whitespace and delimiter in whitespace:
                             pass
                         else:
                             tokens.append({'type':delimiter_type,'block_left':None,'block_right':None,'data':delimiter})
@@ -1258,7 +1258,7 @@ class tokenizer:
                 word+=text[string_index]
             string_index+=1
         if word!='':
-            tokens.append({'block_left':None,'block_right':None,'data':word})
+            tokens.append({'type':'data','block_left':None,'block_right':None,'data':word})
             word=''
         print ("DONE")
         self.info("-[Tokens]----------------")
