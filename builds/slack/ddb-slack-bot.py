6b6be964d16961b7af93b381dc9dd3ddfc61221e
Watkins, Charles <charles.watkins@etrade.com>
2019-08-08 10:13:33 -0400

    Bump Version 1.2.934

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.933'
+__version__='1.2.934'
 
         
 # ############################################################################
@@ -2785,6 +2785,7 @@ class engine:
         p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         output, err = p.communicate()
         rc = p.returncode
+        self.info("OSCMD INFO","{0}".format(" ".join(cmd)))
         self.info("OSCMD INFO","{0}".format(output),"{0}".format(err))
         if rc!=0:
             self.info(output)
