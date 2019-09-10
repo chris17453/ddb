4733cae45be6528950833ef06583c2d26d117d57
Watkins, Charles <charles.watkins@etrade.com>
2019-09-10 13:00:59 -0400

    Bump Version 1.2.957

--- source/ddb/file_io/locking.py
+++ source/ddb/file_io/locking.py
@@ -166,9 +166,9 @@ def remove_temp_file(path):
         #print "Removing temp copy"
         os.remove(path)
         if os.path.exists(path)==True:
-            raise Exception("Failed to delete: {0}".format(path))    
+            raise Exception("Lock, remove temp file failed to delete: {0}".format(path))    
     except Exception as ex:
-        raise Exception("Temp File Remove Lock: {0}".format(ex))
+        raise Exception("Lock, Temp File Remove Lock: {0}".format(ex))
 
         
 # todo move into context with a manager flag        
@@ -202,7 +202,7 @@ def swap_files(path, temp,key_uuid):
             raise Exception("Deleting temp file {0} failed".format(temp))
         
     except Exception as ex:
-        raise Exception("File Error: {0}".format(ex))
+        raise Exception("Locking Swap File Error: {0}".format(ex))
  
 def normalize_path(path):
     """Update a relative or user absed path to an ABS path"""
