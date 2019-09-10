e9e76f7ce94b9acf6d32dd2fd1179360b4395e70
Watkins, Charles <charles.watkins@etrade.com>
2019-09-10 15:47:25 -0400

    update

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.976'
+__version__='1.2.977'
 
         
 # ############################################################################
@@ -2450,9 +2450,13 @@ class table:
                 self.ordinals[v.data.name] = v.data.ordinal
     def delete(self):
         try:
+            print "DT MAN"
             if os.path.exists(self.data.config):
+                print "table exists",self.data.config
                 if os.path.isfile(self.data.config):
+                    print "table is file",self.data.config
                     os.remove(self.data.config)
+                    print "table removed",self.data.config
                 else:
                     err_msg="Table config is not a file! {1}:{0}:{3}".format(self.data.name,self.data.database,self.data.config)
                     raise Exception (err_msg)
