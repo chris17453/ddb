6f2664b71e34d5e209fb86aa040da0b620da54a4
Charles Watkins <charles@titandws.com>
2019-09-12 22:40:56 -0400

    Bump Version 1.2.1010

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -129,7 +129,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.1009'
+__version__='1.2.1010'
 
         
 # ############################################################################
@@ -4329,6 +4329,7 @@ class lock:
             lock.info("Lock","None-Fall Through")
             return lock.LOCK_NONE
         except Exception as ex:
+            return lock.LOCK_OTHER
             lock.info("Lock","Failed to validate file lock: {0}".format(ex))
     @staticmethod
     def release(path):
--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.1009'
+__version__='1.2.1010'
 
         
 # ############################################################################
@@ -4243,6 +4243,7 @@ class lock:
             lock.info("Lock","None-Fall Through")
             return lock.LOCK_NONE
         except Exception as ex:
+            return lock.LOCK_OTHER
             lock.info("Lock","Failed to validate file lock: {0}".format(ex))
     @staticmethod
     def release(path):
