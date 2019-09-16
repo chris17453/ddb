9c557b78683e2c4804308f99815b52943141048a
Watkins, Charles <charles.watkins@etrade.com>
2019-09-16 13:51:20 -0400

    Bump Version 1.2.1019

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.1018'
+__version__='1.2.1019'
 
         
 # ############################################################################
@@ -4308,31 +4308,25 @@ def remove_temp_file(path):
         lock.info("Lock","Removing temp copy: {0}".format(path))
         os.remove(path)
         if os.path.exists(path)==True:
-            raise Exception("Lock, remove temp file failed to delete: {0}".format(path))    
-    except Exception as ex:
-        raise Exception("Lock, Temp File Remove Lock: {0}".format(ex))
+            raise Exception("failed to delete: {0}".format(path))    
+    except OSError as ex: 
+        print ex
+        raise Exception("Lock, Delete file  failed: {0}".format(ex))
 def swap_files(path, temp,key_uuid):
     """ Swap a temporary file with a regular file, by deleting the regular file, and copying the temp to its location """
-    try:
         lock_status=lock.is_locked(path,key_uuid)
         lock.info("Lock","Status: {0}".format(lock_status))
         if lock.LOCK_OWNER != lock_status:
             raise Exception("Cannot swap files, expected lock. Didnt find one {0}".format(path))
         norm_path=normalize_path(path)
         if os.path.exists(norm_path)==True:
-            os.remove(norm_path)
-        if os.path.exists(norm_path)==True:
-            raise Exception("Deleting file {0} failed".format(norm_path))
+            lock.remove_temp_file(norm_path)
         lock.release(path)
         lock.info("Lock","Copying temp to master")
         shutil.copy2(temp, norm_path)
-        lock.info("Lock","Removing temp swap file")
-        os.remove(temp)
+        lock.remove_temp_file(temp)
         if os.path.exists(temp)==True:
             raise Exception("Deleting temp file {0} failed".format(temp))
-    except Exception as ex:
-        print ex
-        raise Exception("Locking Swap File Error: {0}".format(ex))
 def normalize_path(path):
     """Update a relative or user absed path to an ABS path"""
     normalized_path=os.path.abspath(os.path.expanduser(path))
