1443dee588824a875b5ae402e5d00a9cccfc2c66
Watkins, Charles <charles.watkins@etrade.com>
2019-09-16 13:43:23 -0400

    Bump Version 1.2.1017

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.1016'
+__version__='1.2.1017'
 
         
 # ############################################################################
@@ -4332,6 +4332,7 @@ def swap_files(path, temp,key_uuid):
         if os.path.exists(temp)==True:
             raise Exception("Deleting temp file {0} failed".format(temp))
     except Exception as ex:
+        print ex
         raise Exception("Locking Swap File Error: {0}".format(ex))
 def normalize_path(path):
     """Update a relative or user absed path to an ABS path"""
--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ from subprocess import Popen,PIPE
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.2.1016'
+__version__='1.2.1017'
 
         
 # ############################################################################
@@ -4324,6 +4324,7 @@ def swap_files(path, temp,key_uuid):
         if os.path.exists(temp)==True:
             raise Exception("Deleting temp file {0} failed".format(temp))
     except Exception as ex:
+        print ex
         raise Exception("Locking Swap File Error: {0}".format(ex))
 def normalize_path(path):
     """Update a relative or user absed path to an ABS path"""
