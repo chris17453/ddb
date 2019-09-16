d5bc59886011c4e74241a0b59e5d61a1572bfeda
Watkins, Charles <charles.watkins@etrade.com>
2019-09-16 13:07:57 -0400

    Bump Version 1.2.1012

--- source/ddb/file_io/locking.py
+++ source/ddb/file_io/locking.py
@@ -126,7 +126,7 @@ class lock:
         while 1:
             lock_status=lock.is_locked(path,key_uuid,lock_path)
             if lock_status==lock.LOCK_NONE:
-                lock.info("Lock","Creating, {0}".format(path))
+                lock.info("Lock","Creating Lock for {0}".format(path))
                 try:
                     fd=os.open(lock_path, os.O_WRONLY | os.O_CREAT | os.O_EXCL)
                     os.write(fd,lock_contents)
