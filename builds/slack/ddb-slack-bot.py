3a279c8fa8ce7a4617e5c7de329f4d7b16821d8f
Watkins, Charles <charles.watkins@etrade.com>
2019-09-17 12:34:54 -0400

    Bump Version 1.3.101

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.3.100'
+__version__='1.3.101'
 
         
 # ############################################################################
@@ -4371,7 +4371,6 @@ def swap_files(path, temp,key_uuid):
     while compare_files(temp,norm_path)==None:
         lock.error("Lock HASH","Files do not match: {0},{1}".format(temp,norm_path))
         time.sleep(.001)
-    lock.info("Lock HASH","Files do not match: {0},{1}".format(temp,norm_path))
     lock.release(path)
     remove_temp_file(temp)
 def normalize_path(path):
