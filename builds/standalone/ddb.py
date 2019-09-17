2b4b338634907621180cda461a6d51a6c0114b40
Charles Watkins <charles@titandws.com>
2019-09-16 22:28:09 -0400

    Bump Version 1.3.40

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.3.39'
+__version__='1.3.40'
 
         
 # ############################################################################
@@ -4169,7 +4169,7 @@ class lock:
     LOCK_OTHER=2
     LOCK_PARTIAL=3
     debug=None
-    def copy_file(src, dst, buffer_size=10485760, perserveFileDate=True):
+    def copy_file(src, dst, buffer_size=16*1024, perserveFileDate=True):
         '''
         Copies a file to a new location. Much faster performance than Apache Commons due to use of larger buffer
         @param src:    Source File
