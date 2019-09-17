b60af08146640e13e68a6a5d4688616e05c011bd
Charles Watkins <charles@titandws.com>
2019-09-17 01:24:19 -0400

    Bump Version 1.3.79

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -42,7 +42,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.3.78'
+__version__='1.3.79'
 
         
 # ############################################################################
@@ -4348,7 +4348,7 @@ def swap_files(path, temp,key_uuid):
     norm_path=normalize_path(path)
     if os.path.exists(norm_path)==True:
         remove_temp_file(norm_path)
-    if lock.debug: lock.info("Lock","Copying temp to master")
+    if lock.debug: lock.info("Lock","Copying temp to master {0} <- {1 }".format(norm_path,temp))
     lock.copy_file(temp, norm_path)
     lock.release(path)
 def normalize_path(path):
