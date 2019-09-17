07e641fdc9ca24f7e563f6e51b7863c772d46a99
Charles Watkins <charles@titandws.com>
2019-09-17 02:05:07 -0400

    Bump Version 1.3.90

--- builds/slack/ddb-slack-bot.py
+++ builds/slack/ddb-slack-bot.py
@@ -43,7 +43,7 @@ logging.basicConfig()
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.3.89'
+__version__='1.3.90'
 
         
 # ############################################################################
@@ -4317,6 +4317,7 @@ def create_temporary_copy(path,uuid,prefix='ddb_'):
     """ Create a copy of a regular file in a temporary directory """
     try:
         lock.aquire(path,uuid)
+        time.sleep(.001)
         temp_dir = tempfile.gettempdir()
         temp_base_name=next(tempfile._get_candidate_names())+"UUID-"+uuid
         if prefix:
