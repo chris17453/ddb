2493d3ef34d19e7810aa1805d5e1e60b556f34f9
Charles Watkins <charles@titandws.com>
2019-09-16 22:59:50 -0400

    Bump Version 1.3.48

--- builds/standalone/ddb-core.py
+++ builds/standalone/ddb-core.py
@@ -35,7 +35,7 @@ from subprocess import Popen,PIPE
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.3.47'
+__version__='1.3.48'
 
         
 # ############################################################################
@@ -4161,7 +4161,7 @@ class lock:
     LOCK_OWNER=1
     LOCK_OTHER=2
     LOCK_PARTIAL=3
-    debug=None
+    debug=1
     @staticmethod
     def copy_file(src, dst, buffer_size=10485760, perserveFileDate=None):
         '''
