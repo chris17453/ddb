82ee9eae37361882c7fa28c58bf5994a905e68a8
Charles Watkins <charles@titandws.com>
2019-09-18 12:21:00 -0400

    Bump Version 1.3.182

--- test/context.py
+++ test/context.py
@@ -1,5 +1,10 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..','builds','standalone')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..','builds')))
 
-from source import ddb
+try:
+    from standalone import ddb
+except Exception as ex:
+    print ex
+    exit(1)
+print ("DDB STANDALONE")
