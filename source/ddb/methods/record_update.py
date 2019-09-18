6aad61018edf126526c5f55960a14f57edcb33e5
Charles Watkins <charles@titandws.com>
2019-09-18 09:59:58 -0400

    Bump Version 1.3.170

--- source/ddb/methods/record_update.py
+++ source/ddb/methods/record_update.py
@@ -61,9 +61,9 @@ def method_update(context, meta):
         visible_errors    =meta.table.visible.errors
 
 
-        with open(temp_data_file, 'r') as content_file:
+        with open(temp_data_file, 'r', buffering=0) as content_file:
             dst_temp_filename=temp_path_from_file(meta.table.data.path,"ddb_DST_UPDATE",unique=True)
-            with open (dst_temp_filename,"w") as  temp_file:
+            with open (dst_temp_filename,"w", buffering=0) as  temp_file:
       
                 for line in content_file:
                     processed_line = process_line3(context,meta, line, line_number,column_count,delimiter,visible_whitespace,visible_comments, visible_errors)
