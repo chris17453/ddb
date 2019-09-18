5fbc9a056930b7b867a49af12f90a8d176bcf48d
Charles Watkins <charles@titandws.com>
2019-09-18 08:43:21 -0400

    Bump Version 1.3.145

--- source/ddb/file_io/locking.py
+++ source/ddb/file_io/locking.py
@@ -186,7 +186,7 @@ class lock:
             if lock.debug: lock.info('lock',"% s removed successfully" % path) 
         except : 
             ex = sys.exc_info()
-            if lock.debug: lock.error('Lock',"File path can not be removed {0):{1}:{2}".format(ex[0] , ex[1] , ex[2]))
+            if lock.debug: lock.error('Lock',"File path can not be removed {0}:{1}:{2}".format(ex[0] , ex[1] , ex[2]))
             if lock.debug: lock.error('Lock release',ex)
             exit(1)
 
@@ -257,7 +257,7 @@ def create_temporary_copy(path,uuid,prefix='ddb_'):
         return temp_path
     except:
         ex = sys.exc_info()
-        if lock.debug: lock.error("Lock Error","{0):{1}:{2}".format(ex[0] , ex[1] , ex[2]))
+        if lock.debug: lock.error("Lock Error","{0}:{1}:{2}".format(ex[0] , ex[1] , ex[2]))
         exit(1)
         raise Exception("Temp File Create Copy Error: {0}".format(ex))
 
