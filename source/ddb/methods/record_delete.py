5616654cc0f2744d3bcd97aa90a466387533f0ae
Charles Watkins <chris17453@gmail.com>
2019-12-18 15:17:52 -0500

    Bump Version 1.4.72

--- source/ddb/methods/record_delete.py
+++ source/ddb/methods/record_delete.py
@@ -23,7 +23,7 @@ def method_delete(context, meta):
         
         with open(temp_data_file, 'rb', buffering=0) as content_file:
             dst_temp_filename=temp_path_from_file(meta.table.data.path,"ddb_DST_DELETE",unique=True)
-            with open (dst_temp_filename,"w", buffering=0) as  temp_file:
+            with open (dst_temp_filename,"wb", buffering=0) as  temp_file:
 
                 for line in content_file:
                     processed_line = process_line3(context,meta, line, line_number,column_count,delimiter,visible_whitespace,visible_comments, visible_errors)
