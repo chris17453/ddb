b0c3db43a7d091acbb53caf003afce5e4608a6c7
Charles Watkins <chris17453@gmail.com>
2019-12-18 13:44:08 -0500

    Bump Version 1.4.51

--- source/ddb/file_io/locking.py
+++ source/ddb/file_io/locking.py
@@ -248,25 +248,25 @@ def create_temporary_copy(path,uuid='',prefix='ddb_'):
         # it checks for a lock file in the temp dir
         # and blocks this thread/.process until MAX timout occures
         # or the lock ages and is deleted
-        lock.info("1")
+        lock.info("x","1")
         lock.aquire(path,uuid)
-        lock.info("2")
+        lock.info("x","2")
         #time.sleep(.001)
         if lock.debug: lock.info("LOCK Modified",os.stat(path).st_mtime)
-        lock.info("3")
+        lock.info("x","3")
         
         temp_path=temp_path_from_file(path,prefix+uuid)
-        lock.info("4")
+        lock.info("x","4")
         
         norm_path=normalize_path(path)
-        lock.ingo("5")
+        lock.ingo("x","5")
         
         if lock.debug: lock.info("Lock","Creating temporary file: {0}-> {1}".format(norm_path, temp_path))
         #lock.copy_file( norm_path, temp_path)
-        lock.info("6")
+        lock.info("x","6")
         
         shutil.copy2(norm_path, temp_path)
-        lock.info("7")
+        lock.info("x","7")
         
          #print("Deleting: {0} Copying to Deleted: {1}".format(path,temp_path))
         if lock.debug: lock.info("Lock","Created temporary file: {0}".format( temp_path))
