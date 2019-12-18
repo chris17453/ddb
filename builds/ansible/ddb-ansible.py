4d5f1a103008ec4a478f01d0e36bb0a6e2f3da1b
Charles Watkins <chris17453@gmail.com>
2019-12-18 16:01:34 -0500

    Bump Version 1.4.99

--- builds/ansible/ddb-ansible.py
+++ builds/ansible/ddb-ansible.py
@@ -131,7 +131,7 @@ def run_module():
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.4.98'
+__version__='1.4.99'
 
         
 # ############################################################################
@@ -3217,6 +3217,8 @@ def method_delete(context, meta):
             with open (dst_temp_filename,"wb", buffering=0) as  temp_file:
                 for line in content_file:
                     processed_line = process_line3(context,meta, line, line_number,column_count,delimiter,visible_whitespace,visible_comments, visible_errors)
+                    if None != processed_line['error']:
+                        context.add_error(processed_line['error'])
             context.autocommit_write(meta.table,dst_temp_filename)
         context.auto_commit(meta.table)
         return  query_results(success=True,affected_rows=affected_rows,diff=diff)
