1386e417cc07c679e3a75bcf9260efa3abbc7f9f
Charles Watkins <chris17453@gmail.com>
2019-12-18 15:20:43 -0500

    Bump Version 1.4.74

--- builds/standalone/ddb.py
+++ builds/standalone/ddb.py
@@ -45,7 +45,7 @@ from os.path import expanduser
 # File   : ./source/ddb/version.py
 # ############################################################################
 
-__version__='1.4.73'
+__version__='1.4.74'
 
         
 # ############################################################################
@@ -3138,8 +3138,8 @@ def method_delete(context, meta):
                         affected_rows += 1
                         diff.append("Deleted Line: {0}, {1}".format(line_number-1,line))
                         continue
-                    temp_file.write(processed_line['raw'])
-                    temp_file.write(meta.table.delimiters.get_new_line())
+                    temp_file.write(str.encode(processed_line['raw']))
+                    temp_file.write(str.enode(meta.table.delimiters.get_new_line()))
             context.autocommit_write(meta.table,dst_temp_filename)
         context.auto_commit(meta.table)
         return  query_results(success=True,affected_rows=affected_rows,diff=diff)
